7ae452fdfad85c329a00736b5f7e724e9f558639
Dillon Kearns <dillon@dillonkearns.com>
2019-08-09 12:19:07 -0700

    Point to apple touch icon.

--- examples/docs/src/js/helper.ts
+++ examples/docs/src/js/helper.ts
@@ -10,6 +10,26 @@ export function pagesInit({ mainElmModule, imageAssets }: any) {
           appendTag(headTag);
         });
       }
+
+      // <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon-180x180.png">
+      // <link rel="apple-touch-icon" sizes="1024x1024" href="/apple-touch-icon-1024x1024.png">
+      appendTag({
+        name: "link",
+        attributes: [
+          ["rel", "apple-touch-icon"],
+          ["sizes", "180x180"],
+          ["href", "/apple-touch-icon-180x180.png"]
+        ]
+      });
+      appendTag({
+        name: "link",
+        attributes: [
+          ["rel", "apple-touch-icon"],
+          ["sizes", "1024x1024"],
+          ["href", "/apple-touch-icon-1024x1024.png"]
+        ]
+      });
+
       appendTag({
         name: "link",
         attributes: [["rel", "manifest"], ["href", "/manifest.webmanifest"]]
@@ -29,3 +49,17 @@ export function pagesInit({ mainElmModule, imageAssets }: any) {
     document.getElementsByTagName("head")[0].appendChild(meta);
   }
 }
+/*
+<link rel="apple-touch-icon" sizes="57x57" href="/apple-touch-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="/apple-touch-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="/apple-touch-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="/apple-touch-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="/apple-touch-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="167x167" href="/apple-touch-icon-167x167.png">
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon-180x180.png">
+<link rel="apple-touch-icon" sizes="1024x1024" href="/apple-touch-icon-1024x1024.png">
+
+*/
