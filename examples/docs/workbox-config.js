c1d42c05f248b90c765ee637112df1ae3c531ca0
Dillon Kearns <dillon@dillonkearns.com>
2019-08-08 21:26:59 -0700

    Add workbox config.

---
+++ examples/docs/workbox-config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  globDirectory: "dist/",
+  globPatterns: ["**/*.{html,js}"],
+  swDest: "sw.js",
+  runtimeCaching: [
+    {
+      // Match any request that ends with .png, .jpg, .jpeg or .svg.
+      // urlPattern: /^https:\/\/fonts\.googleapis\.com/,
+      urlPattern: /^https:\/\/fonts\.gstatic\.com/,
+
+      // Apply a cache-first strategy.
+      handler: "CacheFirst",
+
+      options: {
+        // Use a custom cache name.
+        cacheName: "fonts"
+      }
+    }
+  ]
+};
