8af4fac3de450881abd9449ebbaa1bb8bb337b1f
Dillon Kearns <dillon@dillonkearns.com>
2019-08-10 10:01:45 -0700

    Add gradient to SVG definition.

--- examples/docs/src/DocumentSvg.elm
+++ examples/docs/src/DocumentSvg.elm
@@ -1,8 +1,34 @@
 module DocumentSvg exposing (view)
 
+import Color
 import Html exposing (Html)
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
+
+
+strokeColor =
+    -- "url(#grad1)"
+    "black"
+
+
+pageTextColor =
+    "black"
+
+
+fillColor =
+    -- "url(#grad1)"
+    "none"
+
+
+gradient color1 color2 =
+    linearGradient [ id "grad1", x1 "0%", y1 "0%", x2 "100%", y2 "0%" ]
+        [ stop
+            [ offset "40%"
+            , Svg.Attributes.style ("stop-color:" ++ Color.toCssString color1 ++ ";stop-opacity:1")
+            ]
+            []
+        , stop [ offset "100%", Svg.Attributes.style ("stop-color:" ++ Color.toCssString color2 ++ ";stop-opacity:1") ] []
+        ]
 
 
 view : Html msg
@@ -13,12 +39,13 @@ view =
         , width "56.114286"
         , height "74.5"
         ]
-        [ defs [] []
-        , metadata [] [ text " Produced by OmniGraffle 7.11.1 " ]
+        [ defs []
+            [ fillGradient ]
+        , metadata [] []
         , g
             [ id "Canvas_11"
             , stroke "none"
-            , fill "none"
+            , fill fillColor
             , strokeOpacity "1"
             , fillOpacity "1"
             , strokeDasharray "none"
@@ -28,52 +55,24 @@ view =
                     [ g [ id "Graphic_32" ]
                         [ Svg.path
                             [ d "M 252.5485 146.02063 L 252.5485 217.52063 L 305.66277 217.52063 L 305.66277 161.68254 L 290.00087 146.02063 Z"
-                            , stroke "black"
+                            , stroke strokeColor
                             , strokeLinecap "round"
                             , strokeLinejoin "round"
                             , strokeWidth "3"
                             ]
                             []
                         ]
-                    , g [ id "Line_34" ] [ line [ x1 "266.07286", y1 "182.8279", x2 "290.75465", y2 "183.00997", stroke "black", strokeLinecap "round", strokeLinejoin "round", strokeWidth "2" ] [] ]
-                    , g [ id "Line_35" ] [ line [ x1 "266.07286", y1 "191.84156", x2 "290.75465", y2 "192.02363", stroke "black", strokeLinecap "round", strokeLinejoin "round", strokeWidth "2" ] [] ]
-                    , g [ id "Line_36" ] [ line [ x1 "266.07286", y1 "200.85522", x2 "290.75465", y2 "201.0373", stroke "black", strokeLinecap "round", strokeLinejoin "round", strokeWidth "2" ] [] ]
-                    , g [ id "Line_37" ] [ line [ x1 "266.07286", y1 "164.80058", x2 "278.3874", y2 "164.94049", stroke "black", strokeLinecap "round", strokeLinejoin "round", strokeWidth "2" ] [] ]
+                    , g [ id "Line_34" ] [ line [ x1 "266.07286", y1 "182.8279", x2 "290.75465", y2 "183.00997", stroke pageTextColor, strokeLinecap "round", strokeLinejoin "round", strokeWidth "2" ] [] ]
+                    , g [ id "Line_35" ] [ line [ x1 "266.07286", y1 "191.84156", x2 "290.75465", y2 "192.02363", stroke pageTextColor, strokeLinecap "round", strokeLinejoin "round", strokeWidth "2" ] [] ]
+                    , g [ id "Line_36" ] [ line [ x1 "266.07286", y1 "200.85522", x2 "290.75465", y2 "201.0373", stroke pageTextColor, strokeLinecap "round", strokeLinejoin "round", strokeWidth "2" ] [] ]
+                    , g [ id "Line_37" ] [ line [ x1 "266.07286", y1 "164.80058", x2 "278.3874", y2 "164.94049", stroke pageTextColor, strokeLinecap "round", strokeLinejoin "round", strokeWidth "2" ] [] ]
                     ]
                 ]
             ]
         ]
 
 
-
--- <?xml version="1.0" encoding="UTF-8" standalone="no"?>
--- <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
--- <svg version="1.1" xmlns:xl="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg" xmlns:dc="http://purl.org/dc/elements/1.1/" viewBox="251.0485 144.52063 56.114286 74.5" width="56.114286" height="74.5">
---   <defs/>
---   <metadata> Produced by OmniGraffle 7.11.1
---     <dc:date>2019-08-10 15:54:02 +0000</dc:date>
---   </metadata>
---   <g id="Canvas_11" stroke="none" fill="none" stroke-opacity="1" fill-opacity="1" stroke-dasharray="none">
---     <title>Canvas 11</title>
---     <g id="Canvas_11: Layer 1">
---       <title>Layer 1</title>
---       <g id="Group_38">
---         <g id="Graphic_32">
---           <path d="M 252.5485 146.02063 L 252.5485 217.52063 L 305.66277 217.52063 L 305.66277 161.68254 L 290.00087 146.02063 Z" stroke="black" stroke-linecap="round" stroke-linejoin="round" stroke-width="3"/>
---         </g>
---         <g id="Line_34">
---           <line x1="266.07286" y1="182.8279" x2="290.75465" y2="183.00997" stroke="black" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/>
---         </g>
---         <g id="Line_35">
---           <line x1="266.07286" y1="191.84156" x2="290.75465" y2="192.02363" stroke="black" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/>
---         </g>
---         <g id="Line_36">
---           <line x1="266.07286" y1="200.85522" x2="290.75465" y2="201.0373" stroke="black" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/>
---         </g>
---         <g id="Line_37">
---           <line x1="266.07286" y1="164.80058" x2="278.3874" y2="164.94049" stroke="black" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/>
---         </g>
---       </g>
---     </g>
---   </g>
--- </svg>
+fillGradient =
+    gradient
+        (Color.rgb255 255 93 194)
+        (Color.rgb255 255 150 250)
