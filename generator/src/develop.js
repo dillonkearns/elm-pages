77617f388c53699a60f64db2249f4eba575c4cb2
Dillon Kearns <dillon@dillonkearns.com>
2019-08-13 16:02:58 -0700

    Add basic boilerplate for running webpack dev server.

--- generator/src/develop.js
+++ generator/src/develop.js
@@ -4,14 +4,19 @@ const middleware = require("webpack-dev-middleware");
 const express = require("express");
 const path = require("path");
 
-export function start() {
+module.exports = { start };
+function start() {
   const compiler = webpack({
     // webpack options
-    entry: "index.html",
+    // entry: "index.html",
+    entry: "./index.html",
     plugins: [],
+    output: {
+      publicPath: "/"
+    },
     resolve: {
       modules: [path.join(__dirname, "src"), "node_modules"],
-      extensions: [".js", ".elm", ".scss", ".png"]
+      extensions: [".js", ".elm", ".scss", ".png", ".html"]
     },
     module: {
       rules: [
@@ -48,7 +53,7 @@ export function start() {
           loader: "file-loader"
         },
         {
-          test: /\.(jpe?g|png|gif|svg)$/i,
+          test: /\.(jpe?g|png|gif|svg|html)$/i,
           exclude: [/elm-stuff/, /node_modules/],
           loader: "file-loader"
         },
@@ -70,10 +75,24 @@ export function start() {
       ]
     }
   });
+  // compiler.watch({}, (error, stats) => {
+  //   if (error) {
+  //     console.log("error", error);
+  //   } else {
+  //     console.log("Running!", stats);
+  //   }
+  // });
+
   const app = express();
 
   app.use(middleware(compiler, { publicPath: "/" }));
   // webpack-dev-middleware options
-
+  app.get("/", (req, res) => {
+    return res.sendFile(path.join(__dirname, "index.html"));
+  });
   app.listen(3000, () => console.log("Example listening on port 3000!"));
+  // https://stackoverflow.com/questions/43667102/webpack-dev-middleware-and-static-files
+  app.use(express.static(__dirname + "/path-to-static-folder"));
+
+  // compiler.run();
 }
