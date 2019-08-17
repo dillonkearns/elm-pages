4973e3985056396e5676dd21ef69408e879a048e
Dillon Kearns <dillon@dillonkearns.com>
2019-08-16 18:35:07 -0700

    Remove unused constant.

--- generator/src/develop.js
+++ generator/src/develop.js
@@ -10,39 +10,6 @@ const { GenerateSW } = require("workbox-webpack-plugin");
 const FaviconsWebpackPlugin = require("favicons-webpack-plugin");
 const AddAssetPlugin = require("add-asset-webpack-plugin");
 
-const aboutContent = `# About elm-conf
-
-elm-conf is a single-day, one-track conference for the Elm programming language community, now in its fourth year.
-Thank you to everyone who helps make elm-conf US a reality: speakers, attendees, the talk selection team, and our sponsors.
-
-Special thanks to [Strange Loop](http://www.thestrangeloop.com) for giving us space and time to put on the conference.
-It wouldn't have been possible without you!
-
-## General organizing principles
-
-1. **A thriving community is built on diversity.**
-   We encourage people from all backgrounds and walks of life to attend and speak.
-   To ensure that everyone is included, we follow and enforce a [code of conduct](http://thestrangeloop.com/policies.html).
-
-2. **We assume you know some Elm already.**
-   Beginner talks, while great, are more appropriate for a general-interest conference.
-
-3. **We cannot have a good conference without good speakers.**
-   We will take care of our speakers leading up to and during the conference and will make sure they have what they need to deliver their best talk.
-   Speakers have the final say on their representation in published materials, on the website, and in their published talks.
-
-3. **elm-conf is for the community.**
-   A conference can't exist without a healthy community.
-   We will do our part by choosing our words carefully and kindly, and communicating early and often about difficulties.
-
-## Organizers
-
-- Brian Hicks
-- Luke Westby
-- Emma Cunningham
-- D Pham
-`;
-
 class AddFilesPlugin {
   constructor(filesList) {
     this.filesList = filesList;
