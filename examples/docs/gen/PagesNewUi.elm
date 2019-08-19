c022531ff27d2dea6dc06310981c7c893075f4be
Dillon Kearns <dillon@dillonkearns.com>
2019-08-19 10:18:56 -0700

    Clean up hand-generated file.

--- examples/docs/gen/PagesNewUi.elm
+++ examples/docs/gen/PagesNewUi.elm
@@ -15,33 +15,12 @@ import RawContent
 port toJsPort : Json.Encode.Value -> Cmd msg
 
 
-type alias Flags =
-    Json.Decode.Value
-
-
-type alias Parser metadata view =
-    Dict String String
-    -> List String
-    -> List ( List String, metadata )
-    -> Mark.Document (Page metadata view)
-
-
-type alias Content =
-    { markdown : List ( List String, { frontMatter : String, body : Maybe String } )
-    , markup : List ( List String, String )
-    }
-
-
-type Model userModel userMsg metadata view
-    = Model
-
-
 application :
     { init : ( userModel, Cmd userMsg )
     , update : userMsg -> userModel -> ( userModel, Cmd userMsg )
     , subscriptions : userModel -> Sub userMsg
     , view : userModel -> List ( List String, metadata ) -> Page metadata view -> { title : String, body : Html userMsg }
-    , parser : Parser metadata view
+    , parser : Pages.Parser metadata view
     , head : metadata -> List Head.Tag
     , frontmatterParser : Json.Decode.Decoder metadata
     , markdownToHtml : String -> view
@@ -56,14 +35,9 @@ application config =
         , subscriptions = config.subscriptions
         , parser = config.parser
         , frontmatterParser = config.frontmatterParser
-        , content = content
+        , content = RawContent.content
         , markdownToHtml = config.markdownToHtml
         , toJsPort = toJsPort
         , head = config.head
         , manifest = config.manifest
         }
-
-
-content : Content
-content =
-    RawContent.content
