47ff28050c1aa46c167bfc93798415420c55c62a
Dillon Kearns <dillon@dillonkearns.com>
2019-08-19 16:25:30 -0700

    Extract constant to module.

--- generator/src/generate-elm-stuff.js
+++ generator/src/generate-elm-stuff.js
@@ -1,51 +1,7 @@
 const fs = require("fs");
 const runElm = require("./compile-elm.js");
 const copyModifiedElmJson = require("./rewrite-elm-json.js");
-
-const elmPagesCliFile = `port module PagesNew exposing (application)
-
-import Dict exposing (Dict)
-import Head
-import Html exposing (Html)
-import Json.Decode
-import Json.Encode
-import Mark
-import Pages
-import Pages.ContentCache exposing (Page)
-import Pages.Manifest
-import RawContent
-
-
-port toJsPort : Json.Encode.Value -> Cmd msg
-
-
-application :
-    { init : ( userModel, Cmd userMsg )
-    , update : userMsg -> userModel -> ( userModel, Cmd userMsg )
-    , subscriptions : userModel -> Sub userMsg
-    , view : userModel -> List ( List String, metadata ) -> Page metadata view -> { title : String, body : Html userMsg }
-    , parser : Pages.Parser metadata view
-    , head : metadata -> List Head.Tag
-    , frontmatterParser : Json.Decode.Decoder metadata
-    , markdownToHtml : String -> view
-    , manifest : Pages.Manifest.Config
-    }
-    -> Pages.Program userModel userMsg metadata view
-application config =
-    Pages.cliApplication
-        { init = config.init
-        , view = config.view
-        , update = config.update
-        , subscriptions = config.subscriptions
-        , parser = config.parser
-        , frontmatterParser = config.frontmatterParser
-        , content = RawContent.content
-        , markdownToHtml = config.markdownToHtml
-        , toJsPort = toJsPort
-        , head = config.head
-        , manifest = config.manifest
-        }
-`;
+const { elmPagesCliFile } = require("./elm-file-constants.js");
 
 module.exports = function run(rawContentFile, callback) {
   // mkdir -p elm-stuff/elm-pages/
