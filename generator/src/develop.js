5e96ba697d59c0f67526f703bc0d113d61660ad3
Dillon Kearns <dillon@dillonkearns.com>
2019-08-21 21:59:06 -0700

    Add caching rule to speed up subsequent imagemin builds.

--- generator/src/develop.js
+++ generator/src/develop.js
@@ -111,6 +111,7 @@ function webpackOptions(production, routes, { debug, manifestConfig }) {
       ]),
       new ImageminPlugin({
         test: /\.(jpe?g|png|gif|svg)$/i,
+        cacheFolder: path.resolve(process.cwd(), "./cache"),
         disable: !production,
         pngquant: {
           quality: "50-70",
