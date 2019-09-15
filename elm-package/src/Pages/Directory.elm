3070f583730b7dd86c81857286d8ac81f66d839f
Dillon Kearns <dillon@dillonkearns.com>
2019-09-15 15:39:26 -0700

    Update docs.

--- elm-package/src/Pages/Directory.elm
+++ elm-package/src/Pages/Directory.elm
@@ -1,8 +1,28 @@
-module Pages.Directory exposing (Directory, WithIndex, WithoutIndex, includes, indexPath, withIndex, withoutIndex)
+module Pages.Directory exposing
+    ( Directory
+    , includes
+    , indexPath, WithIndex, WithoutIndex
+    , withIndex, withoutIndex
+    )
 
 {-| The `Directory` type can be used to get the children of a given directory,
 check if a `PagePath` is within a `Directory`, or get the index page corresponding
 to the directory (if it is a `Directory pathKey WithIndex`).
+
+@docs Directory
+
+@docs includes
+
+
+## Getting a `Directory`'s corresponding index page
+
+@docs indexPath, WithIndex, WithoutIndex
+
+
+## Functions for use by generated code
+
+@docs withIndex, withoutIndex
+
 -}
 
 import Pages.PagePath as PagePath exposing (PagePath)
@@ -80,6 +100,21 @@ includes (Directory key allPagePaths directoryPath) pagePath =
         |> List.member pagePath
 
 
+{-| Get the `PagePath` for the given Directory.
+
+    import Pages
+    import Pages.Directory as Directory exposing (Directory)
+
+    blogDirectory : Directory Pages.PathKey Directory.WithIndex
+    blogDirectory =
+        Pages.pages.blog.directory
+
+    -- blogDirectory |> Directory.indexPath |> PagePath.toString
+    -- => "/blog"
+
+See `Directory.includes` for an example of this in action.
+
+-}
 indexPath : Directory key WithIndex -> PagePath key
 indexPath (Directory key allPagePaths directoryPath) =
     PagePath.build key directoryPath
@@ -91,11 +126,17 @@ toString rawPath =
         ++ (rawPath |> String.join "/")
 
 
+{-| Used by the generated `Pages.elm` module. There's no need to use this
+outside of the generated code.
+-}
 withIndex : key -> List (PagePath key) -> List String -> Directory key WithIndex
 withIndex key allPagePaths path =
     Directory key allPagePaths path
 
 
+{-| Used by the generated `Pages.elm` module. There's no need to use this
+outside of the generated code.
+-}
 withoutIndex : key -> List (PagePath key) -> List String -> Directory key WithoutIndex
 withoutIndex key allPagePaths path =
     Directory key allPagePaths path
