ca101aec90a6d6b04dd2ed89698d97f557b0f5a0
Dillon Kearns <dillon@dillonkearns.com>
2019-09-15 11:23:20 -0700

    Reference local project in example docs.

--- examples/docs/index.js
+++ examples/docs/index.js
@@ -2,7 +2,8 @@ import "elm-oembed";
 import "./lib/code-editor.js";
 import "./style.css";
 const { Elm } = require("./src/Main.elm");
-const pagesInit = require("elm-pages");
+// const pagesInit = require("elm-pages");
+const pagesInit = require("../../index.js");
 
 pagesInit({
   mainElmModule: Elm.Main
