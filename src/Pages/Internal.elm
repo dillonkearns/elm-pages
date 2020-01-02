4c9ac5afcfb30964ad2005c7a0c3fc8603b11cc0
Dillon Kearns <dillon@dillonkearns.com>
2020-01-02 15:28:25 -0800

    Update docs.

--- src/Pages/Internal.elm
+++ src/Pages/Internal.elm
@@ -3,7 +3,9 @@ module Pages.Internal exposing
     , ApplicationType(..)
     )
 
-{-| TODO
+{-| You don't need to use this unless you want to play around with the internals of `elm-pages` to build
+a similar framework and hook into the low-level details. Otherwise, just grab the `Pages.Internal.Internal` value
+that is in the generated `Pages` module (see <Pages.Platform>).
 
 @docs Internal
 
@@ -15,14 +17,14 @@ import Json.Encode
 import Pages.Internal.Platform
 
 
-{-| TODO
+{-| Internal detial to track whether to run the CLI step or the runtime step in the browser.
 -}
 type ApplicationType
     = Browser
     | Cli
 
 
-{-| TODO
+{-| This type is generated for you in your `gen/Pages.elm` module (see <Pages.Platform>).
 -}
 type alias Internal pathKey =
     { applicationType : ApplicationType
