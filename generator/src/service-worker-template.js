fdaaf02d88f653e4db6ed82e032c64dc4d762ded
Dillon Kearns <dillon@dillonkearns.com>
2020-02-03 19:04:13 -0800

    Set network fallback as the NavigationRoute.

--- generator/src/service-worker-template.js
+++ generator/src/service-worker-template.js
@@ -5,30 +5,26 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest);
 
 // This section is based on the following workbox recipe:
 // https://developers.google.com/web/tools/workbox/guides/advanced-recipes#provide_a_fallback_response_to_a_route
+// and this stackoverflow answer: https://stackoverflow.com/a/53597107
 const CACHE_NAME = 'shell';
 const FALLBACK_HTML_URL = '/index.html';
 
-workbox.routing.setCatchHandler(({event}) => {
-  // The FALLBACK_URL entries must be added to the cache ahead of time, either
-  // via runtime or precaching. If they are precached, then call
-  // `matchPrecache(FALLBACK_URL)` (from the `workbox-precaching` package)
-  // to get the response from the correct cache.
-  //
-  // Use event, request, and url to figure out how to respond.
-  // One approach would be to use request.destination, see
-  // https://medium.com/dev-channel/service-worker-caching-strategies-based-on-request-types-57411dd7652c
-  switch (event.request.destination) {
-    case 'document':
-      return caches.match(FALLBACK_HTML_URL, {
-        cacheName: CACHE_NAME,
-      });
-    break;
+let networkOnly = new workbox.strategies.NetworkOnly();
 
-    default:
-      // If we don't have a fallback, just return an error response.
-      return Response.error();
+const navigationHandler = async (args) => {
+  try {
+    const response = await networkOnly.handle(args);
+    return response || await caches.match(FALLBACK_HTML_URL);
+  } catch (error) {
+    return await caches.match(FALLBACK_HTML_URL);
   }
-});
+};
+
+workbox.routing.registerRoute(
+  new workbox.routing.NavigationRoute(navigationHandler)
+);
+
+
 
 self.addEventListener('install', async (event) => {
   event.waitUntil(
