f329a14bd9263774c3e863aa939f8c6dcfb25abb
Dillon Kearns <dillon@dillonkearns.com>
2020-05-17 10:12:03 -0700

    Add snapshot test for generated Pages.elm file.

--- tests/example.test.js
+++ tests/example.test.js
@@ -1,3 +1,9 @@
-test('adds 1 + 2 to equal 3', () => {
-    expect(1 + 2).toBe(3);
+const { elmPagesCliFile, elmPagesUiFile } = require("../generator/src/elm-file-constants.js");
+const generateRecords = require("../generator/src/generate-records.js");
+
+test('generate UI file', () => {
+    const staticRoutes = generateRecords();
+
+    global.builtAt = new Date("Sun, 17 May 2020 16:53:22 GMT");
+    expect(elmPagesUiFile(staticRoutes, [])).toMatchSnapshot();
 });
