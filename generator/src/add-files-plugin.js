04e4b7add096c90a3300eca0ea46a52045fa94c0
Dillon Kearns <dillon@dillonkearns.com>
2020-06-07 20:13:33 -0700

    Format line with prettier.

--- generator/src/add-files-plugin.js
+++ generator/src/add-files-plugin.js
@@ -2,7 +2,7 @@ const path = require("path");
 const fs = require("fs");
 const globby = require("globby");
 const parseFrontmatter = require("./frontmatter.js");
-const webpack = require('webpack')
+const webpack = require("webpack");
 
 function unpackFile(filePath) {
   const { content, data } = parseFrontmatter(
@@ -17,44 +17,41 @@ function unpackFile(filePath) {
   return {
     baseRoute,
     content,
-    filePath
+    filePath,
   };
 }
 
 module.exports = class AddFilesPlugin {
   apply(/** @type {webpack.Compiler} */ compiler) {
-
-    (global.mode === "dev" ? compiler.hooks.emit : compiler.hooks.make).tapAsync("AddFilesPlugin", (compilation, callback) => {
-
-
+    (global.mode === "dev"
+      ? compiler.hooks.emit
+      : compiler.hooks.make
+    ).tapAsync("AddFilesPlugin", (compilation, callback) => {
       const files = globby.sync("content").map(unpackFile);
 
-
-      let staticRequestData = {}
-      global.pagesWithRequests.then(payload => {
-
-        if (payload.type === 'error') {
-          compilation.errors.push(new Error(payload.message))
-        } else if (payload.errors && payload.errors.length > 0) {
-          compilation.errors.push(new Error(payload.errors[0]))
-        }
-        else {
-          staticRequestData = payload.pages
-        }
-      })
+      let staticRequestData = {};
+      global.pagesWithRequests
+        .then((payload) => {
+          if (payload.type === "error") {
+            compilation.errors.push(new Error(payload.message));
+          } else if (payload.errors && payload.errors.length > 0) {
+            compilation.errors.push(new Error(payload.errors[0]));
+          } else {
+            staticRequestData = payload.pages;
+          }
+        })
         .finally(() => {
-
-          files.forEach(file => {
+          files.forEach((file) => {
             // Couldn't find this documented in the webpack docs,
             // but I found the example code for it here:
             // https://github.com/jantimon/html-webpack-plugin/blob/35a154186501fba3ecddb819b6f632556d37a58f/index.js#L470-L478
 
-            let route = file.baseRoute.replace(/\/$/, '');
+            let route = file.baseRoute.replace(/\/$/, "");
             const staticRequests = staticRequestData[route];
 
             const filename = path.join(file.baseRoute, "content.json");
             if (compilation.contextDependencies) {
-              compilation.contextDependencies.add('content')
+              compilation.contextDependencies.add("content");
             }
             // compilation.fileDependencies.add(filename);
             if (compilation.fileDependencies) {
@@ -62,29 +59,27 @@ module.exports = class AddFilesPlugin {
             }
             const rawContents = JSON.stringify({
               body: file.content,
-              staticData: staticRequests || {}
+              staticData: staticRequests || {},
             });
 
             compilation.assets[filename] = {
               source: () => rawContents,
-              size: () => rawContents.length
+              size: () => rawContents.length,
             };
           });
 
-          (global.filesToGenerate || []).forEach(file => {
+          (global.filesToGenerate || []).forEach((file) => {
             // Couldn't find this documented in the webpack docs,
             // but I found the example code for it here:
             // https://github.com/jantimon/html-webpack-plugin/blob/35a154186501fba3ecddb819b6f632556d37a58f/index.js#L470-L478
             compilation.assets[file.path] = {
               source: () => file.content,
-              size: () => file.content.length
+              size: () => file.content.length,
             };
           });
 
-          callback()
-
-        })
-
+          callback();
+        });
     });
   }
 };
