2ff04b7afc88a7917fb80292d9b7b70b6be7993f
Dillon Kearns <dillon@dillonkearns.com>
2020-06-17 17:25:55 -0700

    Run snapshot tests for each directory as a test case.

--- tests/generated-files.test.js
+++ tests/generated-files.test.js
@@ -1,10 +1,22 @@
 const { elmPagesCliFile, elmPagesUiFile } = require("../generator/src/elm-file-constants.js");
 const generateRecords = require("../generator/src/generate-records.js");
+const path = require('path');
 
-test('generate UI file', async () => {
-    process.chdir(__dirname);
-    const staticRoutes = await generateRecords();
+const { readdirSync } = require('fs');
+global.builtAt = new Date("Sun, 17 May 2020 16:53:22 GMT");
 
-    global.builtAt = new Date("Sun, 17 May 2020 16:53:22 GMT");
-    expect(elmPagesUiFile(staticRoutes, [])).toMatchSnapshot();
-});
+const getDirectories = (/** @type {string} */ source) =>
+    readdirSync(source, { withFileTypes: true })
+        .filter(dirent => dirent.isDirectory())
+        .map(dirent => dirent.name)
+
+getDirectories(path.join(__dirname, 'snapshot-cases')).forEach(snapshotDir => {
+
+    test(`generate UI file ${snapshotDir}`, async () => {
+        process.chdir(path.join(__dirname, 'snapshot-cases', snapshotDir));
+        const staticRoutes = await generateRecords();
+        expect(elmPagesUiFile(staticRoutes, [])).toMatchSnapshot();
+    });
+
+
+})
