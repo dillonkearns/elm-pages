17aa244fc6ec11733b6666a903951f9f432a7726
Dillon Kearns <dillon@dillonkearns.com>
2020-10-16 15:59:43 -0700

    Run terser with npx.

--- generator/src/cli.js
+++ generator/src/cli.js
@@ -146,7 +146,7 @@ async function compileElm() {
  */
 async function runTerser(filePath) {
   await shellCommand(
-    `terser ${filePath} --module --compress 'pure_funcs="F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9",pure_getters,keep_fargs=false,unsafe_comps,unsafe' | terser --module --mangle --output=${filePath}`
+    `npx terser ${filePath} --module --compress 'pure_funcs="F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9",pure_getters,keep_fargs=false,unsafe_comps,unsafe' | npx terser --module --mangle --output=${filePath}`
   );
 }
 
