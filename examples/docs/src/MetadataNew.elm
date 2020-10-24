28692cf0031c85ea44e39374aa3738d15fa82e82
Dillon Kearns <dillon@dillonkearns.com>
2020-10-24 15:55:45 -0700

    Use new decoder in template module decoder.

--- examples/docs/src/MetadataNew.elm
+++ examples/docs/src/MetadataNew.elm
@@ -1,6 +1,11 @@
 module MetadataNew exposing (DocMetadata, PageMetadata, decoder)
 
+import Cloudinary
+import Data.Author
+import Date exposing (Date)
 import Json.Decode as Decode exposing (Decoder)
+import Pages
+import Pages.ImagePath exposing (ImagePath)
 import Template.BlogPost
 import Template.Page
 import Template.Showcase
@@ -14,6 +19,16 @@ type alias DocMetadata =
 
 type alias PageMetadata =
     { title : String }
+
+
+type alias ArticleMetadata =
+    { title : String
+    , description : String
+    , published : Date
+    , author : Data.Author.Author
+    , image : ImagePath Pages.PathKey
+    , draft : Bool
+    }
 
 
 decoder : Decoder TemplateType
@@ -39,9 +54,36 @@ decoder =
                             |> Decode.map TemplateType.Showcase
 
                     "blog" ->
-                        Template.BlogPost.decoder
+                        Decode.map6 ArticleMetadata
+                            (Decode.field "title" Decode.string)
+                            (Decode.field "description" Decode.string)
+                            (Decode.field "published"
+                                (Decode.string
+                                    |> Decode.andThen
+                                        (\isoString ->
+                                            case Date.fromIsoString isoString of
+                                                Ok date ->
+                                                    Decode.succeed date
+
+                                                Err error ->
+                                                    Decode.fail error
+                                        )
+                                )
+                            )
+                            (Decode.field "author" Data.Author.decoder)
+                            (Decode.field "image" imageDecoder)
+                            (Decode.field "draft" Decode.bool
+                                |> Decode.maybe
+                                |> Decode.map (Maybe.withDefault False)
+                            )
                             |> Decode.map TemplateType.BlogPost
 
                     _ ->
                         Decode.fail <| "Unexpected page \"type\" " ++ pageType
             )
+
+
+imageDecoder : Decoder (ImagePath Pages.PathKey)
+imageDecoder =
+    Decode.string
+        |> Decode.map (\cloudinaryAsset -> Cloudinary.url cloudinaryAsset Nothing 800)
