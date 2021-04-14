36cf57d937bf380edf090d059dfcbeaa67e5eebf
Dillon Kearns <dillon@dillonkearns.com>
2021-04-14 15:56:36 -0700

    Fix error in slides index.js.

--- examples/slides/static/index.js
+++ examples/slides/static/index.js
@@ -1,8 +1,1 @@
-import * as elmOembed from "/elm-oembed.js";
-// import "./lib/native-shim.js";
-
-export default function (elmLoaded) {
-  document.addEventListener("DOMContentLoaded", function (event) {
-    elmOembed.setup();
-  });
-}
+export default function (elmLoaded) {}
