2fdecc4bde6d9315934076f7f7bf8dce34a8fa12
Dillon Kearns <dillon@dillonkearns.com>
2021-04-17 15:13:41 -0700

    Setup dev server as CLI command.

--- generator/src/cli.js
+++ generator/src/cli.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 const build = require("./build.js");
+const dev = require("./dev-server.js");
 
 const commander = require("commander");
 
@@ -9,13 +10,23 @@ const packageVersion = require("../../package.json").version;
 async function main() {
   const program = new commander.Command();
 
+  program.version(packageVersion);
+
   program
-    .version(packageVersion)
     .command("build")
     .option("--debug", "Skip terser and run elm make with --debug")
     .description("run a full site build")
     .action(async (options) => {
       await build.run(options);
+    });
+
+  program
+    .command("dev")
+    .description("start a dev server")
+    .option("--port <number>", "serve site at localhost:<port>", "1234")
+    .action(async (options) => {
+      console.log({ options });
+      await dev.start(options);
     });
 
   program.parse(process.argv);
--- generator/src/dev-server.js
+++ generator/src/dev-server.js
@@ -3,7 +3,6 @@ const fs = require("fs");
 const chokidar = require("chokidar");
 const compiledElmPath = path.join(process.cwd(), "elm-stuff/elm-pages/elm.js");
 const renderer = require("../../generator/src/render");
-const port = 1234;
 const { spawnElmMake, compileElmForBrowser } = require("./compile-elm.js");
 const http = require("http");
 const codegen = require("./codegen.js");
@@ -11,196 +10,202 @@ const kleur = require("kleur");
 const serveStatic = require("serve-static");
 const connect = require("connect");
 
-global.staticHttpCache = {};
-let elmMakeRunning = true;
+async function start(options) {
+  const port = options.port;
+  global.staticHttpCache = {};
+  let elmMakeRunning = true;
 
-const serve = serveStatic("static/", { index: false });
-const generatedFilesDirectory = "elm-stuff/elm-pages/generated-files";
-fs.mkdirSync(generatedFilesDirectory, { recursive: true });
-const serveGeneratedFiles = serveStatic(generatedFilesDirectory, {
-  index: false,
-});
-const serveStaticCode = serveStatic(path.join(__dirname, "../static-code"), {});
-/** @type {{ id: number, response: http.ServerResponse }[]} */
-let clients = [];
-
-// TODO check source-directories for what to watch?
-const watcher = chokidar.watch(["elm.json"], {
-  persistent: true,
-  ignored: [/\.swp$/],
-  ignoreInitial: true,
-});
-watchElmSourceDirs();
-let clientElmMakeProcess = compileElmForBrowser();
-let pendingCliCompile = compileCliApp();
-
-async function setup() {
-  await codegen.generate();
-  await Promise.all([clientElmMakeProcess, pendingCliCompile])
-    .then(() => {
-      console.log("Dev server ready");
-      elmMakeRunning = false;
-    })
-    .catch(() => {
-      elmMakeRunning = false;
-    });
-}
-
-setup();
-
-function watchElmSourceDirs() {
-  console.log("elm.json changed - reloading watchers");
-  watcher.removeAllListeners();
-  const sourceDirs = JSON.parse(fs.readFileSync("./elm.json").toString())[
-    "source-directories"
-  ];
-  console.log("Watching...", { sourceDirs });
-  watcher.add(sourceDirs);
-}
-
-async function compileCliApp() {
-  await spawnElmMake(
-    "TemplateModulesBeta.elm",
-    "elm.js",
-    "elm-stuff/elm-pages"
+  const serve = serveStatic("static/", { index: false });
+  const generatedFilesDirectory = "elm-stuff/elm-pages/generated-files";
+  fs.mkdirSync(generatedFilesDirectory, { recursive: true });
+  const serveGeneratedFiles = serveStatic(generatedFilesDirectory, {
+    index: false,
+  });
+  const serveStaticCode = serveStatic(
+    path.join(__dirname, "../static-code"),
+    {}
   );
-}
+  /** @type {{ id: number, response: http.ServerResponse }[]} */
+  let clients = [];
 
-const app = connect()
-  .use(timeMiddleware())
-  .use(processRequest)
-  .use(serveStaticCode)
-  .use(serveGeneratedFiles)
-  .use(serve);
-http.createServer(app).listen(port);
-/**
- * @param {http.IncomingMessage} request
- * @param {http.ServerResponse} response
- * @param {connect.NextHandleFunction} next
- */
-async function processRequest(request, response, next) {
-  if (request.url?.startsWith("/elm.js")) {
-    try {
-      const clientElmJs = await clientElmMakeProcess;
-      response.writeHead(200, { "Content-Type": "text/javascript" });
-      response.end(clientElmJs);
-    } catch (elmCompilerError) {
-      response.writeHead(500, { "Content-Type": "application/json" });
-      response.end(elmCompilerError);
-    }
-  } else if (request.url?.startsWith("/stream")) {
-    handleStream(request, response);
-  } else if (
-    request.url.includes("content.json") ||
-    request.headers["sec-fetch-mode"] === "navigate"
-  ) {
-    handleNavigationRequest(request, response);
-  } else {
-    next();
+  // TODO check source-directories for what to watch?
+  const watcher = chokidar.watch(["elm.json"], {
+    persistent: true,
+    ignored: [/\.swp$/],
+    ignoreInitial: true,
+  });
+  watchElmSourceDirs();
+  let clientElmMakeProcess = compileElmForBrowser();
+  let pendingCliCompile = compileCliApp();
+
+  async function setup() {
+    await codegen.generate();
+    await Promise.all([clientElmMakeProcess, pendingCliCompile])
+      .then(() => {
+        console.log("Dev server ready");
+        elmMakeRunning = false;
+      })
+      .catch(() => {
+        elmMakeRunning = false;
+      });
   }
-}
 
-console.log(`elm-pages dev server running at http://localhost:${port}`);
+  setup();
 
-watcher.on("all", async function (eventName, pathThatChanged) {
-  console.log({ pathThatChanged });
-  if (pathThatChanged === "elm.json") {
-    watchElmSourceDirs();
-  } else if (pathThatChanged.endsWith(".elm")) {
-    if (elmMakeRunning) {
-    } else {
-      if (needToRerunCodegen(eventName, pathThatChanged)) {
-        await codegen.generate();
+  function watchElmSourceDirs() {
+    console.log("elm.json changed - reloading watchers");
+    watcher.removeAllListeners();
+    const sourceDirs = JSON.parse(fs.readFileSync("./elm.json").toString())[
+      "source-directories"
+    ];
+    console.log("Watching...", { sourceDirs });
+    watcher.add(sourceDirs);
+  }
+
+  async function compileCliApp() {
+    await spawnElmMake(
+      "TemplateModulesBeta.elm",
+      "elm.js",
+      "elm-stuff/elm-pages"
+    );
+  }
+
+  const app = connect()
+    .use(timeMiddleware())
+    .use(processRequest)
+    .use(serveStaticCode)
+    .use(serveGeneratedFiles)
+    .use(serve);
+  http.createServer(app).listen(port);
+  /**
+   * @param {http.IncomingMessage} request
+   * @param {http.ServerResponse} response
+   * @param {connect.NextHandleFunction} next
+   */
+  async function processRequest(request, response, next) {
+    if (request.url?.startsWith("/elm.js")) {
+      try {
+        const clientElmJs = await clientElmMakeProcess;
+        response.writeHead(200, { "Content-Type": "text/javascript" });
+        response.end(clientElmJs);
+      } catch (elmCompilerError) {
+        response.writeHead(500, { "Content-Type": "application/json" });
+        response.end(elmCompilerError);
       }
-      elmMakeRunning = true;
-      clientElmMakeProcess = compileElmForBrowser();
-      pendingCliCompile = compileCliApp();
-      let timestamp = Date.now();
+    } else if (request.url?.startsWith("/stream")) {
+      handleStream(request, response);
+    } else if (
+      request.url.includes("content.json") ||
+      request.headers["sec-fetch-mode"] === "navigate"
+    ) {
+      handleNavigationRequest(request, response);
+    } else {
+      next();
+    }
+  }
 
-      Promise.all([clientElmMakeProcess, pendingCliCompile])
-        .then(() => {
-          elmMakeRunning = false;
-        })
-        .catch(() => {
-          elmMakeRunning = false;
+  console.log(`elm-pages dev server running at http://localhost:${port}`);
+
+  watcher.on("all", async function (eventName, pathThatChanged) {
+    console.log({ pathThatChanged });
+    if (pathThatChanged === "elm.json") {
+      watchElmSourceDirs();
+    } else if (pathThatChanged.endsWith(".elm")) {
+      if (elmMakeRunning) {
+      } else {
+        if (needToRerunCodegen(eventName, pathThatChanged)) {
+          await codegen.generate();
+        }
+        elmMakeRunning = true;
+        clientElmMakeProcess = compileElmForBrowser();
+        pendingCliCompile = compileCliApp();
+        let timestamp = Date.now();
+
+        Promise.all([clientElmMakeProcess, pendingCliCompile])
+          .then(() => {
+            elmMakeRunning = false;
+          })
+          .catch(() => {
+            elmMakeRunning = false;
+          });
+        clients.forEach((client) => {
+          client.response.write(`data: content.json\n\n`);
         });
+      }
+    } else {
+      const changedPathRelative = path.relative(process.cwd(), pathThatChanged);
+
+      Object.keys(global.staticHttpCache).forEach((dataSourceKey) => {
+        if (dataSourceKey.includes(`file://${changedPathRelative}`)) {
+          delete global.staticHttpCache[dataSourceKey];
+        }
+      });
       clients.forEach((client) => {
         client.response.write(`data: content.json\n\n`);
       });
     }
-  } else {
-    const changedPathRelative = path.relative(process.cwd(), pathThatChanged);
+  });
 
-    Object.keys(global.staticHttpCache).forEach((dataSourceKey) => {
-      if (dataSourceKey.includes(`file://${changedPathRelative}`)) {
-        delete global.staticHttpCache[dataSourceKey];
-      }
+  /**
+   * @param {http.IncomingMessage} request
+   * @param {http.ServerResponse} response
+   */
+  function handleStream(request, response) {
+    response.writeHead(200, {
+      Connection: "keep-alive",
+      "Content-Type": "text/event-stream",
     });
-    clients.forEach((client) => {
-      client.response.write(`data: content.json\n\n`);
+    const clientId = Date.now();
+    clients.push({ id: clientId, response });
+    request.on("close", () => {
+      clients = clients.filter((client) => client.id !== clientId);
     });
   }
-});
 
-/**
- * @param {http.IncomingMessage} request
- * @param {http.ServerResponse} response
- */
-function handleStream(request, response) {
-  response.writeHead(200, {
-    Connection: "keep-alive",
-    "Content-Type": "text/event-stream",
-  });
-  const clientId = Date.now();
-  clients.push({ id: clientId, response });
-  request.on("close", () => {
-    clients = clients.filter((client) => client.id !== clientId);
-  });
-}
+  /**
+   * @param {string} pathThatChanged
+   * @param {'add' | 'unlink' | 'addDir' | 'unlinkDir' | 'change'} eventName
+   */
+  function needToRerunCodegen(eventName, pathThatChanged) {
+    return (
+      (eventName === "add" || eventName === "unlink") &&
+      pathThatChanged.match(/src\/Template\/.*\.elm/)
+    );
+  }
 
-/**
- * @param {string} pathThatChanged
- * @param {'add' | 'unlink' | 'addDir' | 'unlinkDir' | 'change'} eventName
- */
-function needToRerunCodegen(eventName, pathThatChanged) {
-  return (
-    (eventName === "add" || eventName === "unlink") &&
-    pathThatChanged.match(/src\/Template\/.*\.elm/)
-  );
-}
-
-async function handleNavigationRequest(req, res) {
-  if (req.url.endsWith(".ico") || req.url.endsWith("manifest.json")) {
-    res.writeHead(404, {
-      "Content-Type": "text/html",
-    });
-    res.end(`Not found.`);
-  } else {
-    try {
-      await pendingCliCompile;
-      const renderResult = await renderer(
-        compiledElmPath,
-        req.url,
-        req,
-        function (pattern) {
-          console.log(`Watching data source ${pattern}`);
-          watcher.add(pattern);
+  async function handleNavigationRequest(req, res) {
+    if (req.url.endsWith(".ico") || req.url.endsWith("manifest.json")) {
+      res.writeHead(404, {
+        "Content-Type": "text/html",
+      });
+      res.end(`Not found.`);
+    } else {
+      try {
+        await pendingCliCompile;
+        const renderResult = await renderer(
+          compiledElmPath,
+          req.url,
+          req,
+          function (pattern) {
+            console.log(`Watching data source ${pattern}`);
+            watcher.add(pattern);
+          }
+        );
+        if (renderResult.kind === "json") {
+          res.writeHead(200, {
+            "Content-Type": "application/json",
+          });
+          res.end(renderResult.contentJson);
+        } else {
+          res.writeHead(200, {
+            "Content-Type": "text/html",
+          });
+          res.end(renderResult.htmlString);
         }
-      );
-      if (renderResult.kind === "json") {
-        res.writeHead(200, {
-          "Content-Type": "application/json",
-        });
-        res.end(renderResult.contentJson);
-      } else {
-        res.writeHead(200, {
-          "Content-Type": "text/html",
-        });
-        res.end(renderResult.htmlString);
+      } catch (error) {
+        res.writeHead(500, { "Content-Type": "application/json" });
+        res.end(JSON.stringify(error.errorsJson));
       }
-    } catch (error) {
-      res.writeHead(500, { "Content-Type": "application/json" });
-      res.end(JSON.stringify(error.errorsJson));
     }
   }
 }
@@ -244,3 +249,5 @@ function timeFrom(start, subtract = 0) {
     return kleur.red(timeString);
   }
 }
+
+module.exports = { start };
