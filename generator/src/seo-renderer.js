bd2aeee5421e9c73723ad0e5caf1844ac5e55921
Dillon Kearns <dillon@dillonkearns.com>
2021-04-23 07:11:44 -0700

    Remove unused code.

--- generator/src/seo-renderer.js
+++ generator/src/seo-renderer.js
@@ -1,27 +1,6 @@
-const elmPagesVersion = "TODO";
-
 module.exports = { toString };
 
 function toString(/** @type { SeoTag[] }  */ tags) {
-  //   appendTag({
-  //     type: "head",
-  //     name: "meta",
-  //     attributes: [
-  //       ["name", "generator"],
-  //       ["content", `elm-pages v${elmPagesVersion}`],
-  //     ],
-  //   });
-
-  const generatorTag /** @type { HeadTag } */ = {
-    type: "head",
-    name: "meta",
-    attributes: [
-      ["name", "generator"],
-      ["content", `elm-pages v${elmPagesVersion}`],
-    ],
-  };
-  //   tags.concat([generatorTag]);
-
   return tags
     .map((headTag) => {
       if (headTag.type === "head") {
@@ -39,21 +18,14 @@ function toString(/** @type { SeoTag[] }  */ tags) {
 
 /** @typedef {{ name: string; attributes: string[][]; type: 'head' }} HeadTag */
 function appendTag(/** @type {HeadTag} */ tagDetails) {
-  //   const meta = document.createElement(tagDetails.name);
   const tagsString = tagDetails.attributes.map(([name, value]) => {
-    // meta.setAttribute(name, value);
     return `${name}="${value}"`;
   });
   return `    <${tagDetails.name} ${tagsString.join(" ")} />`;
-  //   document.getElementsByTagName("head")[0].appendChild(meta);
 }
 
 /** @typedef {{ contents: Object; type: 'json-ld' }} JsonLdTag */
 function appendJsonLdTag(/** @type {JsonLdTag} */ tagDetails) {
-  //   let jsonLdScript = document.createElement("script");
-  //   jsonLdScript.type = "application/ld+json";
-  //   jsonLdScript.innerHTML = JSON.stringify(tagDetails.contents);
-  //   document.getElementsByTagName("head")[0].appendChild(jsonLdScript);
   return `<script type="application/ld+json">
 ${JSON.stringify(tagDetails.contents)}
 </script>`;
