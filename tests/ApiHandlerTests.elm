f0d64e59f00fc78ec4d91ea85fa684572125dbf0
Dillon Kearns <dillon@dillonkearns.com>
2021-04-29 16:42:13 -0700

    Restore route matching tests.

--- tests/ApiHandlerTests.elm
+++ tests/ApiHandlerTests.elm
@@ -7,38 +7,41 @@ import Test exposing (describe, only, test)
 
 all =
     describe "api routes"
-        [ --test "match top-level file with no extension" <|
-          --    \() ->
-          --        succeed
-          --            (\userId ->
-          --                { body = "Data for user " ++ userId }
-          --            )
-          --            |> captureSegment
-          --            |> tryMatch "123"
-          --            |> Expect.equal (Just { body = "Data for user 123" })
-          --, test "file with extension" <|
-          --    \() ->
-          --        succeed
-          --            (\userId ->
-          --                { body = "Data for user " ++ userId }
-          --            )
-          --            |> captureSegment
-          --            |> literalSegment ".json"
-          --            |> tryMatch "124.json"
-          --            |> Expect.equal (Just { body = "Data for user 124" })
-          --, test "file path with multiple segments" <|
-          --    \() ->
-          --        succeed
-          --            (\userId ->
-          --                { body = "Data for user " ++ userId }
-          --            )
-          --            |> literalSegment "users"
-          --            |> slash
-          --            |> captureSegment
-          --            |> literalSegment ".json"
-          --            |> tryMatch "users/123.json"
-          --            |> Expect.equal (Just { body = "Data for user 123" }),
-          test "routes" <|
+        [ test "match top-level file with no extension" <|
+            \() ->
+                succeed
+                    (\userId ->
+                        { body = "Data for user " ++ userId }
+                    )
+                    (\_ -> [])
+                    |> captureSegment
+                    |> tryMatch "123"
+                    |> Expect.equal (Just { body = "Data for user 123" })
+        , test "file with extension" <|
+            \() ->
+                succeed
+                    (\userId ->
+                        { body = "Data for user " ++ userId }
+                    )
+                    (\_ -> [])
+                    |> captureSegment
+                    |> literalSegment ".json"
+                    |> tryMatch "124.json"
+                    |> Expect.equal (Just { body = "Data for user 124" })
+        , test "file path with multiple segments" <|
+            \() ->
+                succeed
+                    (\userId ->
+                        { body = "Data for user " ++ userId }
+                    )
+                    (\_ -> [])
+                    |> literalSegment "users"
+                    |> slash
+                    |> captureSegment
+                    |> literalSegment ".json"
+                    |> tryMatch "users/123.json"
+                    |> Expect.equal (Just { body = "Data for user 123" })
+        , test "routes" <|
             \() ->
                 routesExample
                     |> withRoutes
