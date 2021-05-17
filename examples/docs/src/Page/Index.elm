95967ebcfe783ead1eb5f3842f0b651c15bd8372
Dillon Kearns <dillon@dillonkearns.com>
2021-05-17 16:59:18 -0700

    Add SEO example to landing page.

--- examples/docs/src/Page/Index.elm
+++ examples/docs/src/Page/Index.elm
@@ -205,6 +205,40 @@ repo repoName =
         )
 """ )
             }
+        , firstSection
+            { heading = "SEO"
+            , body = "Make sure your site previews look polished with the type-safe SEO API. elm-pages build pre-renders HTML for your pages. And your SEO tags get access to the page's DataSources."
+            , buttonText = "Learn about the SEO API"
+            , buttonLink = Route.Docs__Section__ { section = Nothing }
+            , svgIcon = "M10 21h7a2 2 0 002-2V9.414a1 1 0 00-.293-.707l-5.414-5.414A1 1 0 0012.586 3H7a2 2 0 00-2 2v11m0 5l4.879-4.879m0 0a3 3 0 104.243-4.242 3 3 0 00-4.243 4.242z"
+            , code =
+                ( "src/Page/Blog/Slug_.elm", """head :
+    StaticPayload Data RouteParams
+    -> List Head.Tag
+head static =
+    Seo.summaryLarge
+        { canonicalUrlOverride = Nothing
+        , siteName = "elm-pages"
+        , image =
+            { url = static.data.image
+            , alt = static.data.description
+            , dimensions = Nothing
+            , mimeType = Nothing
+            }
+        , description = static.data.description
+        , locale = Nothing
+        , title = static.data.title
+        }
+        |> Seo.article
+            { tags = []
+            , section = Nothing
+            , publishedTime = Just (Date.toIsoString static.data.published)
+            , modifiedTime = Nothing
+            , expirationTime = Nothing
+            }
+
+""" )
+            }
         ]
 
 
