513159cd867fc98c938613abbbb1abd6a2272e90
Dillon Kearns <dillon@dillonkearns.com>
2021-06-25 09:57:05 -0700

    Update index.js page for template.

--- generator/template/public/index.js
+++ generator/template/public/index.js
@@ -1,8 +1,12 @@
+/** @typedef {{load: (Promise<unknown>); flags: (unknown)}} ElmPagesInit */
+
+/** @type ElmPagesInit */
 export default {
-  load: function (elmLoaded) {
-    document.addEventListener("DOMContentLoaded", function (event) {});
+  load: async function (elmLoaded) {
+    const app = await elmLoaded;
+    console.log("App loaded", app);
   },
   flags: function () {
-    return null;
+    return "You can decode this in Shared.elm using Json.Decode.string!";
   },
 };
