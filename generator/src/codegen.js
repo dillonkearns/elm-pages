0dbb2138dbdbc3952321d5aa978676453e518a20
Dillon Kearns <dillon@dillonkearns.com>
2021-07-10 13:56:44 -0700

    Run codegen steps in parallel.

--- generator/src/codegen.js
+++ generator/src/codegen.js
@@ -9,63 +9,62 @@ const { ensureDirSync, deleteIfExists } = require("./file-helpers.js");
 global.builtAt = new Date();
 
 async function generate() {
-  await writeFiles();
-}
-
-async function writeFiles() {
   const cliCode = generateTemplateModuleConnector("cli");
   const browserCode = generateTemplateModuleConnector("browser");
   ensureDirSync("./elm-stuff");
   ensureDirSync("./.elm-pages");
   ensureDirSync("./elm-stuff/elm-pages/.elm-pages");
 
-  fs.copyFileSync(path.join(__dirname, `./Page.elm`), `./.elm-pages/Page.elm`);
-  fs.copyFileSync(
-    path.join(__dirname, `./elm-application.json`),
-    `./elm-stuff/elm-pages/elm-application.json`
-  );
-  fs.copyFileSync(
-    path.join(__dirname, `./Page.elm`),
-    `./elm-stuff/elm-pages/.elm-pages/Page.elm`
-  );
-  fs.copyFileSync(
-    path.join(__dirname, `./SharedTemplate.elm`),
-    `./.elm-pages/SharedTemplate.elm`
-  );
-  fs.copyFileSync(
-    path.join(__dirname, `./SharedTemplate.elm`),
-    `./elm-stuff/elm-pages/.elm-pages/SharedTemplate.elm`
-  );
-  fs.copyFileSync(
-    path.join(__dirname, `./SiteConfig.elm`),
-    `./.elm-pages/SiteConfig.elm`
-  );
-  fs.copyFileSync(
-    path.join(__dirname, `./SiteConfig.elm`),
-    `./elm-stuff/elm-pages/.elm-pages/SiteConfig.elm`
-  );
-
   const uiFileContent = elmPagesUiFile();
-  fs.writeFileSync("./.elm-pages/Pages.elm", uiFileContent);
-
-  // write `Pages.elm` with cli interface
-  fs.writeFileSync(
-    "./elm-stuff/elm-pages/.elm-pages/Pages.elm",
-    elmPagesCliFile()
-  );
-  fs.writeFileSync(
-    "./elm-stuff/elm-pages/.elm-pages/TemplateModulesBeta.elm",
-    cliCode.mainModule
-  );
-  fs.writeFileSync(
-    "./elm-stuff/elm-pages/.elm-pages/Route.elm",
-    cliCode.routesModule
-  );
-  fs.writeFileSync(
-    "./.elm-pages/TemplateModulesBeta.elm",
-    browserCode.mainModule
-  );
-  fs.writeFileSync("./.elm-pages/Route.elm", browserCode.routesModule);
+  await Promise.all([
+    fs.promises.copyFile(
+      path.join(__dirname, `./Page.elm`),
+      `./.elm-pages/Page.elm`
+    ),
+    fs.promises.copyFile(
+      path.join(__dirname, `./elm-application.json`),
+      `./elm-stuff/elm-pages/elm-application.json`
+    ),
+    fs.promises.copyFile(
+      path.join(__dirname, `./Page.elm`),
+      `./elm-stuff/elm-pages/.elm-pages/Page.elm`
+    ),
+    fs.promises.copyFile(
+      path.join(__dirname, `./SharedTemplate.elm`),
+      `./.elm-pages/SharedTemplate.elm`
+    ),
+    fs.promises.copyFile(
+      path.join(__dirname, `./SharedTemplate.elm`),
+      `./elm-stuff/elm-pages/.elm-pages/SharedTemplate.elm`
+    ),
+    fs.promises.copyFile(
+      path.join(__dirname, `./SiteConfig.elm`),
+      `./.elm-pages/SiteConfig.elm`
+    ),
+    fs.promises.copyFile(
+      path.join(__dirname, `./SiteConfig.elm`),
+      `./elm-stuff/elm-pages/.elm-pages/SiteConfig.elm`
+    ),
+    fs.promises.writeFile("./.elm-pages/Pages.elm", uiFileContent),
+    // write `Pages.elm` with cli interface
+    fs.promises.writeFile(
+      "./elm-stuff/elm-pages/.elm-pages/Pages.elm",
+      elmPagesCliFile()
+    ),
+    fs.promises.writeFile(
+      "./elm-stuff/elm-pages/.elm-pages/TemplateModulesBeta.elm",
+      cliCode.mainModule
+    ),
+    fs.promises.writeFile(
+      "./elm-stuff/elm-pages/.elm-pages/Route.elm",
+      cliCode.routesModule
+    ),
+    fs.promises.writeFile(
+      "./.elm-pages/TemplateModulesBeta.elm",
+      browserCode.mainModule
+    ),
+    fs.promises.writeFile("./.elm-pages/Route.elm", browserCode.routesModule),
+  ]);
 
   // write modified elm.json to elm-stuff/elm-pages/
   copyModifiedElmJson();
