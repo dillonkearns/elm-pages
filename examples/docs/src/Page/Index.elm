780e78fe02c7c9aa3d2ae13411de773d82142fd8
Dillon Kearns <dillon@dillonkearns.com>
2021-07-28 17:08:03 -0700

    Update copy.

--- examples/docs/src/Page/Index.elm
+++ examples/docs/src/Page/Index.elm
@@ -245,7 +245,7 @@ head static =
             }
         , firstSection
             { heading = "Optimized Data"
-            , body = "Make sure your site previews look polished with the type-safe SEO API. elm-pages build pre-renders HTML for your pages. And your SEO tags get access to the page's DataSources."
+            , body = "The OptimizedDecoder module is a drop-in replacement for vanilla decoders. Just by using that module, elm-pages will strip out any untouched JSON values from your DataSource's."
             , buttonText = "Learn about OptimizedDecoders"
             , buttonLink = Route.Docs__Section__ { section = Nothing }
             , svgIcon = "M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10"
