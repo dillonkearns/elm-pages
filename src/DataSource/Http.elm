0dd10c3192e6f6289ba2a74c73a07200835e91a1
Dillon Kearns <dillon@dillonkearns.com>
2021-07-29 14:33:20 -0700

    Update references to use DataSource instead of StaticHttp in docs.

--- src/DataSource/Http.elm
+++ src/DataSource/Http.elm
@@ -6,17 +6,17 @@ module DataSource.Http exposing
     , Expect, expectString, expectUnoptimizedJson
     )
 
-{-| StaticHttp requests are an alternative to doing Elm HTTP requests the traditional way using the `elm/http` package.
+{-| `DataSource.Http` requests are an alternative to doing Elm HTTP requests the traditional way using the `elm/http` package.
 
 The key differences are:
 
-  - `StaticHttp.Request`s are performed once at build time (`Http.Request`s are performed at runtime, at whenever point you perform them)
-  - `StaticHttp.Request`s strip out unused JSON data from the data your decoder doesn't touch to minimize the JSON payload
-  - `StaticHttp.Request`s can use [`Pages.Secrets`](Pages.Secrets) to securely use credentials from your environment variables which are completely masked in the production assets.
-  - `StaticHttp.Request`s have a built-in `StaticHttp.andThen` that allows you to perform follow-up requests without using tasks
+  - `DataSource.Http.Request`s are performed once at build time (`Http.Request`s are performed at runtime, at whenever point you perform them)
+  - `DataSource.Http.Request`s strip out unused JSON data from the data your decoder doesn't touch to minimize the JSON payload
+  - `DataSource.Http.Request`s can use [`Pages.Secrets`](Pages.Secrets) to securely use credentials from your environment variables which are completely masked in the production assets.
+  - `DataSource.Http.Request`s have a built-in `DataSource.andThen` that allows you to perform follow-up requests without using tasks
 
 
-## Scenarios where StaticHttp is a good fit
+## Scenarios where DataSource.Http is a good fit
 
 If you need data that is refreshed often you may want to do a traditional HTTP request with the `elm/http` package.
 The kinds of situations that are served well by static HTTP are with data that updates moderately frequently or infrequently (or never).
@@ -26,11 +26,11 @@ you may want to have your site rebuild everytime your calendar feed has an event
 or updated on a CMS service like Contentful.
 
 In scenarios like this, you can serve data that is just as up-to-date as it would be using `elm/http`, but you get the performance
-gains of using `StaticHttp.Request`s as well as the simplicity and robustness that comes with it. Read more about these benefits
-in [this article introducing StaticHttp requests and some concepts around it](https://elm-pages.com/blog/static-http).
+gains of using `DataSource.Http.Request`s as well as the simplicity and robustness that comes with it. Read more about these benefits
+in [this article introducing DataSource.Http requests and some concepts around it](https://elm-pages.com/blog/static-http).
 
 
-## Scenarios where StaticHttp is not a good fit
+## Scenarios where DataSource.Http is not a good fit
 
   - Data that is specific to the logged-in user
   - Data that needs to be the very latest and changes often (for example, sports scores)
@@ -39,7 +39,7 @@ in [this article introducing StaticHttp requests and some concepts around it](ht
 @docs get, request
 
 
-## Building a StaticHttp Request Body
+## Building a DataSource.Http Request Body
 
 The way you build a body is analogous to the `elm/http` package. Currently, only `emptyBody` and
 `stringBody` are supported. If you have a use case that calls for a different body type, please open a Github issue
@@ -52,7 +52,7 @@ and describe your use case!
 
 Warning - use these at your own risk! It's highly recommended that you use the other request functions that make use of
 `zwilias/json-decode-exploration` in order to allow you to reduce down your JSON to only the values that are used by
-your decoders. This can significantly reduce download sizes for your StaticHttp requests.
+your decoders. This can significantly reduce download sizes for your DataSource.Http requests.
 
 @docs unoptimizedRequest
 
@@ -80,14 +80,14 @@ import RequestsAndPending
 import Secrets
 
 
-{-| Build an empty body for a StaticHttp request. See [elm/http's `Http.emptyBody`](https://package.elm-lang.org/packages/elm/http/latest/Http#emptyBody).
+{-| Build an empty body for a DataSource.Http request. See [elm/http's `Http.emptyBody`](https://package.elm-lang.org/packages/elm/http/latest/Http#emptyBody).
 -}
 emptyBody : Body
 emptyBody =
     Body.EmptyBody
 
 
-{-| Builds a string body for a StaticHttp request. See [elm/http's `Http.stringBody`](https://package.elm-lang.org/packages/elm/http/latest/Http#stringBody).
+{-| Builds a string body for a DataSource.Http request. See [elm/http's `Http.stringBody`](https://package.elm-lang.org/packages/elm/http/latest/Http#stringBody).
 
 Note from the `elm/http` docs:
 
@@ -99,27 +99,28 @@ stringBody contentType content =
     Body.StringBody contentType content
 
 
-{-| Builds a JSON body for a StaticHttp request. See [elm/http's `Http.jsonBody`](https://package.elm-lang.org/packages/elm/http/latest/Http#jsonBody).
+{-| Builds a JSON body for a DataSource.Http request. See [elm/http's `Http.jsonBody`](https://package.elm-lang.org/packages/elm/http/latest/Http#jsonBody).
 -}
 jsonBody : Encode.Value -> Body
 jsonBody content =
     Body.JsonBody content
 
 
-{-| A body for a StaticHttp request.
+{-| A body for a DataSource.Http request.
 -}
 type alias Body =
     Body.Body
 
 
-{-| A simplified helper around [`StaticHttp.request`](#request), which builds up a StaticHttp GET request.
+{-| A simplified helper around [`DataSource.Http.request`](#request), which builds up a DataSource.Http GET request.
 
     import DataSource
+    import DataSource.Http
     import Json.Decode as Decode exposing (Decoder)
 
-    getRequest : StaticHttp.Request Int
+    getRequest : DataSource Int
     getRequest =
-        StaticHttp.get
+        DataSource.Http.get
             (Secrets.succeed "https://api.github.com/repos/dillonkearns/elm-pages")
             (Decode.field "stargazers_count" Decode.int)
 
@@ -144,7 +145,7 @@ get url decoder =
         decoder
 
 
-{-| The full details to perform a StaticHttp request.
+{-| The full details to perform a DataSource.Http request.
 -}
 type alias RequestDetails =
     { url : String
@@ -159,9 +160,9 @@ requestToString requestDetails =
     requestDetails.url
 
 
-{-| Build a `StaticHttp` request (analagous to [Http.request](https://package.elm-lang.org/packages/elm/http/latest/Http#request)).
-This function takes in all the details to build a `StaticHttp` request, but you can build your own simplified helper functions
-with this as a low-level detail, or you can use functions like [StaticHttp.get](#get).
+{-| Build a `DataSource.Http` request (analagous to [Http.request](https://package.elm-lang.org/packages/elm/http/latest/Http#request)).
+This function takes in all the details to build a `DataSource.Http` request, but you can build your own simplified helper functions
+with this as a low-level detail, or you can use functions like [DataSource.Http.get](#get).
 -}
 request :
     Pages.Secrets.Value RequestDetails
@@ -172,7 +173,7 @@ request urlWithSecrets decoder =
 
 
 {-| Analgous to the `Expect` type in the `elm/http` package. This represents how you will process the data that comes
-back in your StaticHttp request.
+back in your DataSource.Http request.
 
 You can derive `ExpectUnoptimizedJson` from `ExpectString`. Or you could build your own helper to process the String
 as XML, for example, or give an `elm-pages` build error if the response can't be parsed as XML.
@@ -185,22 +186,22 @@ type Expect value
 
 
 {-| Request a raw String. You can validate the String if you need to check the formatting, or try to parse it
-in something besides JSON. Be sure to use the `StaticHttp.request` function if you want an optimized request that
+in something besides JSON. Be sure to use the `DataSource.Http.request` function if you want an optimized request that
 strips out unused JSON to optimize your asset size.
 
-If the function you pass to `expectString` yields an `Err`, then you will get at StaticHttpDecodingError that will
+If the function you pass to `expectString` yields an `Err`, then you will get a build error that will
 fail your `elm-pages` build and print out the String from the `Err`.
 
     request =
-        StaticHttp.unoptimizedRequest
+        DataSource.Http.unoptimizedRequest
             (Secrets.succeed
                 { url = "https://example.com/file.txt"
                 , method = "GET"
                 , headers = []
-                , body = StaticHttp.emptyBody
+                , body = DataSource.Http.emptyBody
                 }
             )
-            (StaticHttp.expectString
+            (DataSource.Http.expectString
                 (\string ->
                     if String.toUpper string == string then
                         Ok string
@@ -217,11 +218,11 @@ expectString =
 
 
 {-| Handle the incoming response as JSON and don't optimize the asset and strip out unused values.
-Be sure to use the `StaticHttp.request` function if you want an optimized request that
+Be sure to use the `DataSource.Http.request` function if you want an optimized request that
 strips out unused JSON to optimize your asset size. This function makes sense to use for things like a GraphQL request
 where the JSON payload is already trimmed down to the data you explicitly requested.
 
-If the function you pass to `expectString` yields an `Err`, then you will get at StaticHttpDecodingError that will
+If the function you pass to `expectString` yields an `Err`, then you will get a build error that will
 fail your `elm-pages` build and print out the String from the `Err`.
 
 -}
@@ -231,7 +232,7 @@ expectUnoptimizedJson =
 
 
 {-| This is an alternative to the other request functions in this module that doesn't perform any optimizations on the
-asset. Be sure to use the optimized versions, like `StaticHttp.request`, if you can. Using those can significantly reduce
+asset. Be sure to use the optimized versions, like `DataSource.Http.request`, if you can. Using those can significantly reduce
 your asset sizes by removing all unused fields from your JSON.
 
 You may want to use this function instead if you need XML data or plaintext. Or maybe you're hitting a GraphQL API,
