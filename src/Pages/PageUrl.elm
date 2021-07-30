92ed0a37b020cf2571ea5db46e59f41f6bd3ae33
Dillon Kearns <dillon@dillonkearns.com>
2021-07-30 15:49:39 -0700

    Update docs.

--- src/Pages/PageUrl.elm
+++ src/Pages/PageUrl.elm
@@ -1,6 +1,10 @@
 module Pages.PageUrl exposing (PageUrl, toUrl)
 
-{-|
+{-| Same as a Url in `elm/url`, but slightly more structured. The path portion of the URL is parsed into a [`Path`](Path) type, and
+the query params use the [`QueryParams`](QueryParams) type which allows you to parse just the query params or access them into a Dict.
+
+Because `elm-pages` takes care of the main routing for pages in your app, the standard Elm URL parser API isn't suited
+to parsing query params individually, which is why the structure of these types is different.
 
 @docs PageUrl, toUrl
 
