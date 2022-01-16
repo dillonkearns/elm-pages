43a227d22c9ef9179f843cb5b5b269854a471b38
Dillon Kearns <dillon@incrementalelm.com>
2022-01-16 14:31:19 -0800

    Add Form.Value module.

---
+++ src/Form/Value.elm
@@ -0,0 +1,39 @@
+module Form.Value exposing (Value, toString)
+
+import Date exposing (Date)
+
+
+type Value dataType
+    = Value String
+
+
+toString : Value dataType -> String
+toString (Value rawValue) =
+    rawValue
+
+
+date : Date -> Value Date
+date date_ =
+    date_
+        |> Date.toIsoString
+        |> Value
+
+
+float : Float -> Value Float
+float float_ =
+    float_
+        |> String.fromFloat
+        |> Value
+
+
+int : Int -> Value Int
+int int_ =
+    int_
+        |> String.fromInt
+        |> Value
+
+
+string : String -> Value String
+string string_ =
+    string_
+        |> Value
