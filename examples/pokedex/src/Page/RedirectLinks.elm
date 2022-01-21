b7185adc396824fe8e72b332e6a0d805beec61cf
Dillon Kearns <dillon@incrementalelm.com>
2022-01-20 18:01:07 -0800

    Add example page.

---
+++ examples/pokedex/src/Page/RedirectLinks.elm
@@ -0,0 +1,78 @@
+module Page.RedirectLinks exposing (Data, Model, Msg, page)
+
+import DataSource exposing (DataSource)
+import Head
+import Head.Seo as Seo
+import Html
+import Page exposing (Page, PageWithState, StaticPayload)
+import Pages.PageUrl exposing (PageUrl)
+import Pages.Url
+import Route
+import Shared
+import View exposing (View)
+
+
+type alias Model =
+    {}
+
+
+type alias Msg =
+    ()
+
+
+type alias RouteParams =
+    {}
+
+
+page : Page RouteParams Data
+page =
+    Page.single
+        { head = head
+        , data = data
+        }
+        |> Page.buildNoState { view = view }
+
+
+type alias Data =
+    {}
+
+
+data : DataSource Data
+data =
+    DataSource.succeed {}
+
+
+head :
+    StaticPayload Data RouteParams
+    -> List Head.Tag
+head static =
+    Seo.summary
+        { canonicalUrlOverride = Nothing
+        , siteName = "elm-pages"
+        , image =
+            { url = Pages.Url.external "TODO"
+            , alt = "elm-pages logo"
+            , dimensions = Nothing
+            , mimeType = Nothing
+            }
+        , description = "TODO"
+        , locale = Nothing
+        , title = "TODO title" -- metadata.title -- TODO
+        }
+        |> Seo.website
+
+
+view :
+    Maybe PageUrl
+    -> Shared.Model
+    -> StaticPayload Data RouteParams
+    -> View Msg
+view maybeUrl sharedModel static =
+    { title = "Placeholder"
+    , body =
+        [ Route.link
+            Route.Login
+            []
+            [ Html.text "Login" ]
+        ]
+    }
