0964448ae5de4838e6fd46b8a74c28ff135267ae
Dillon Kearns <dillon@incrementalelm.com>
2022-02-21 14:46:51 -0800

    Remove obsolete code.

--- generator/src/pre-render-html.js
+++ generator/src/pre-render-html.js
@@ -1,7 +1,4 @@
-const cliVersion = require("../../package.json").version;
 const seo = require("./seo-renderer.js");
-const elmPagesJsMinified = require("./elm-pages-js-minified.js");
-const path = require("path").posix;
 
 /** @typedef { { head: any[]; errors: any[]; contentJson: any[]; html: string; route: string; title: string; } } Arg */
 /** @typedef { { tag : 'PageProgress'; args : Arg[] } } PageProgress */
@@ -16,16 +13,6 @@ module.exports = function wrapHtml(
   const devServerOnly = (/** @type {string} */ devServerOnlyString) =>
     devServer ? devServerOnlyString : "";
   const seoData = seo.gather(fromElm.head);
-  // return template
-  //   .replace(
-  //     /<!--\s*PLACEHOLDER_HEAD_AND_DATA\s*-->/,
-  //     `${seoData.headTags}
-  //   <script id="__ELM_PAGES_BYTES_DATA__" type="application/octet-stream">${Buffer.from(
-  //     contentDatPayload.buffer
-  //   ).toString("base64")}</script>`
-  //   )
-  //   .replace(/<!--\s*PLACEHOLDER_TITLE\s*-->/, fromElm.title)
-  //   .replace(/<!--\s*PLACEHOLDER_HTML\s* -->/, fromElm.html);
   return {
     kind: "html-template",
     title: fromElm.title,
@@ -35,86 +22,6 @@ module.exports = function wrapHtml(
     rootElement: seoData.rootElement,
   };
 };
-/**
- * @param {string} basePath
- * @param {Arg} fromElm
- * @param {unknown} contentJson
- * @param {boolean} devServer
- * @returns {string}
- */
-function wrapHtmlOld(
-  basePath,
-  fromElm,
-  contentJson,
-  devServer,
-  contentDatPayload
-) {
-  const devServerOnly = (/** @type {string} */ devServerOnlyString) =>
-    devServer ? devServerOnlyString : "";
-  const seoData = seo.gather(fromElm.head);
-  /*html*/
-  return `<!DOCTYPE html>
-  ${seoData.rootElement}
-  <head>
-    <link rel="stylesheet" href="${path.join(basePath, "style.css")}">
-    ${devServerOnly(devServerStyleTag())}
-    <link rel="preload" href="${path.join(basePath, "elm.js")}" as="script">
-    <link rel="modulepreload" href="${path.join(basePath, "index.js")}">
-    ${devServerOnly(
-      /* html */ `<script defer="defer" src="${path.join(
-        basePath,
-        "hmr.js"
-      )}" type="text/javascript"></script>`
-    )}
-    <script defer="defer" src="${path.join(
-      basePath,
-      "elm.js"
-    )}" type="text/javascript"></script>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width,initial-scale=1">
-    <script type="module">
-import userInit from"${path.join(basePath, "index.js")}";
-${elmPagesJsMinified}
-    </script>
-    <title>${fromElm.title}</title>
-    <meta name="generator" content="elm-pages v${cliVersion}">
-    <meta name="mobile-web-app-capable" content="yes">
-    <meta name="theme-color" content="#ffffff">
-    <meta name="apple-mobile-web-app-capable" content="yes">
-    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
-    ${seoData.headTags}
-    <script id="__ELM_PAGES_BYTES_DATA__" type="application/octet-stream">${Buffer.from(
-      contentDatPayload.buffer
-    ).toString("base64")}</script>
-    
-    </head>
-    <body>
-      <div data-url="" display="none"></div>
-      ${fromElm.html}
-    </body>
-  </html>
-  `;
-}
-
-/**
- * @param {string} route
- */
-function cleanRoute(route) {
-  return route.replace(/(^\/|\/$)/, "");
-}
-
-/**
- * @param {string} cleanedRoute
- */
-function pathToRoot(cleanedRoute) {
-  return cleanedRoute === ""
-    ? cleanedRoute
-    : cleanedRoute
-        .split("/")
-        .map((_) => "..")
-        .join("/")
-        .replace(/\.$/, "./");
-}
 
 function devServerStyleTag() {
   /*html*/
