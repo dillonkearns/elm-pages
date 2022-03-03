cc134dc363dcbe43f9b3c7466ab4e3674e73c90c
Dillon Kearns <dillon@incrementalelm.com>
2022-03-03 10:29:43 -0800

    Add todo comment.

--- generator/src/render.js
+++ generator/src/render.js
@@ -326,7 +326,7 @@ async function readFileJobNew(req, patternsToWatch) {
   try {
     patternsToWatch.add(filePath);
 
-    const fileContents = (
+    const fileContents = ( // TODO can I remove this hack?
       await fsPromises.readFile(
         path.join(process.env.LAMBDA_TASK_ROOT || process.cwd(), filePath)
       )
