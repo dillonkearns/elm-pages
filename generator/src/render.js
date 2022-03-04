55ae855f7c6b835fe074ae3ef9bec983ecfce6c5
Dillon Kearns <dillon@incrementalelm.com>
2022-03-04 12:33:04 -0800

    Formatting.

--- generator/src/render.js
+++ generator/src/render.js
@@ -326,8 +326,7 @@ async function readFileJobNew(req, patternsToWatch) {
   try {
     patternsToWatch.add(filePath);
 
-    const fileContents = // TODO can I remove this hack?
-    (
+    const fileContents = ( // TODO can I remove this hack?
       await fsPromises.readFile(
         path.join(process.env.LAMBDA_TASK_ROOT || process.cwd(), filePath)
       )
