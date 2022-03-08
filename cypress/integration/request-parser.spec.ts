d989a9ad9be3b3739f9bbae0de09a31f4ac621a7
Dillon Kearns <dillon@incrementalelm.com>
2022-03-08 15:56:02 -0800

    Add tset case for form POST.

--- cypress/integration/request-parser.spec.ts
+++ cypress/integration/request-parser.spec.ts
@@ -23,3 +23,23 @@ it("empty body", () => {
     expect(res.body.rawBody).to.be.null;
   });
 });
+
+it("form POST", () => {
+  cy.setCookie("session-id", "123");
+  const body = new URLSearchParams({ first: "Jane", last: "Doe" }).toString();
+  cy.request({
+    method: "POST",
+    url: "/api/request-test",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: body,
+  }).then((res) => {
+    expect(res.headers["content-type"]).to.eq("application/json");
+    expect(res.status).to.eq(200);
+    expect(res.body).to.deep.eq({
+      method: "POST",
+      rawBody: body,
+      cookies: { "session-id": "123" },
+      queryParams: {},
+    });
+  });
+});
