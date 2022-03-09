c6933c24976bc924a6e1aeae449a8e0a2f3af875
Dillon Kearns <dillon@incrementalelm.com>
2022-03-09 13:51:35 -0800

    Update test assertion.

--- cypress/integration/elm-pages-dev.spec.js
+++ cypress/integration/elm-pages-dev.spec.js
@@ -9,7 +9,7 @@ context("dev server 404 page", () => {
       failOnStatusCode: false,
     });
     cy.contains(
-      `/blog/non-existent-page successfully matched the route /blog/:slug from the Page Module src/Blog/Slug_.elm`
+      `/blog/non-existent-page successfully matched the route /blog/:slug from the Route Module src/Blog/Slug_.elm`
     );
     cy.contains(
       `But this Page module has no pre-rendered routes! If you want to pre-render this page, add these RouteParams to the module's routes`
