55a1ebe8bcbea8d62f75b62442ee6b369b8d6e7d
Dillon Kearns <dillon@incrementalelm.com>
2022-04-20 11:20:31 -0700

    Clean up cloudflare worker script.

--- examples/hackernews-cloudflare/adapter.mjs
+++ examples/hackernews-cloudflare/adapter.mjs
@@ -30,6 +30,7 @@ export default async function run({
  */
 function rendererCode(isOnDemand, htmlTemplate) {
   return `const htmlTemplate = ${JSON.stringify(htmlTemplate)};
+const devMode = true;
 
 const compiledPortsFile = "../dist/port-data-source.mjs";
 const Elm = require("../dist/elm-pages-cli.js");
@@ -38,94 +39,68 @@ const preRenderHtml = require("../generator/src/pre-render-html.js");
 
 
 export async function onRequest(context) {
-  // Contents of context object
-  const {
-    request, // same as existing Worker API
-    env, // same as existing Worker API
-    params, // if filename includes [id] or [[path]]
-    waitUntil, // same as ctx.waitUntil in existing Worker API
-    next, // used for middleware or to fetch assets
-    data, // arbitrary space for passing data between middlewares
-  } = context;
+  const { request, env, next } = context;
 
-  let { pathname } = new URL(request.url);
-  let res;
-
-  if (pathname.startsWith("/assets") || pathname.startsWith("/elm.")) {
-    console.log("@@@ serving asset", pathname);
-    //     next();
-    //     res = await env.ASSETS.fetch(request);
-
-    //     res = new Response(res.body, {
-    //       headers: {
-    //         // include original cache headers, minus cache-control which
-    //         // is overridden, and etag which is no longer useful
-    //         // "cache-control": "public, immutable, max-age=31536000",
-    //         // "content-type": res.headers.get("content-type"),
-    //         "x-robots-tag": "noindex",
-    //       },
-    //     });
-    //     return res;
-    res = await next();
-    console.log("res", res);
+  let res = await next();
+  if (res.status !== 404) {
     return res;
-  } else {
-    //     return new Response("Hello, world!");
-    try {
-      const requestTime = new Date();
-      const basePath = "/";
-      const mode = "build";
-      const addWatcher = () => {};
+  }
 
-      const renderResult = await renderer(
-        compiledPortsFile,
-        basePath,
-        Elm,
-        mode,
-        new URL(request.url).pathname,
-        await requestToJson(request, requestTime),
-        addWatcher,
-        false,
+  try {
+    const requestTime = new Date();
+    const basePath = "/";
+    const mode = "build";
+    const addWatcher = () => {};
+
+    const renderResult = await renderer(
+      compiledPortsFile,
+      basePath,
+      Elm,
+      mode,
+      new URL(request.url).pathname,
+      await requestToJson(request, requestTime),
+      addWatcher,
+      false,
+      {
+        getEnv: function (name) {
+          return env[name];
+        },
+      }
+    );
+
+    const statusCode = renderResult.is404 ? 404 : renderResult.statusCode;
+
+    if (renderResult.kind === "bytes") {
+      return new Response(renderResult.contentDatPayload.buffer, {
+        headers: {
+          "Content-Type": "application/octet-stream",
+          "x-powered-by": "elm-pages",
+          ...renderResult.headers,
+        },
+        status: statusCode,
+      });
+    } else if (renderResult.kind === "api-response") {
+      const serverResponse = renderResult.body;
+      return new Response(serverResponse.body, {
+        // isBase64Encoded: serverResponse.isBase64Encoded, // TODO check if base64 encoded, if it is then convert base64 string to binary
+        headers: serverResponse.headers,
+        status: serverResponse.statusCode,
+      });
+    } else {
+      return new Response(
+        preRenderHtml.replaceTemplate(htmlTemplate, renderResult.htmlString),
         {
-          getEnv: function (name) {
-            return env[name];
-          },
-        }
-      );
-
-      const statusCode = renderResult.is404 ? 404 : renderResult.statusCode;
-
-      if (renderResult.kind === "bytes") {
-        return new Response(renderResult.contentDatPayload.buffer, {
           headers: {
-            "Content-Type": "application/octet-stream",
+            "Content-Type": "text/html",
             "x-powered-by": "elm-pages",
             ...renderResult.headers,
           },
           status: statusCode,
-        });
-      } else if (renderResult.kind === "api-response") {
-        const serverResponse = renderResult.body;
-        return new Response(serverResponse.body, {
-          // isBase64Encoded: serverResponse.isBase64Encoded, // TODO check if base64 encoded, if it is then convert base64 string to binary
-          headers: serverResponse.headers,
-          status: serverResponse.statusCode,
-        });
-      } else {
-        return new Response(
-          preRenderHtml.replaceTemplate(htmlTemplate, renderResult.htmlString),
-          {
-            headers: {
-              "Content-Type": "text/html",
-              "x-powered-by": "elm-pages",
-              ...renderResult.headers,
-            },
-            status: statusCode,
-          }
-        );
-      }
-    } catch (error) {
-      // console.trace(error);
+        }
+      );
+    }
+  } catch (error) {
+    if (devMode) {
       return new Response(
         \`<body><h1>Error</h1><pre>\${error.toString()}</pre><div><pre>\${
           error.stack
@@ -138,6 +113,14 @@ export async function onRequest(context) {
           },
         }
       );
+    } else {
+      return new Response(\`<body><h1>Internal Error</h1></body>\`, {
+        status: 500,
+        headers: {
+          "Content-Type": "text/html",
+          "x-powered-by": "elm-pages",
+        },
+      });
     }
   }
 }
