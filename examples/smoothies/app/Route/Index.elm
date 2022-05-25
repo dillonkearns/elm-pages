88dc66127a118ab49a551f8f0b46c107d123fb56
Dillon Kearns <dillon@incrementalelm.com>
2022-05-25 09:21:33 -0700

    Update SEO info.

--- examples/smoothies/app/Route/Index.elm
+++ examples/smoothies/app/Route/Index.elm
@@ -256,10 +256,10 @@ head :
 head static =
     Seo.summary
         { canonicalUrlOverride = Nothing
-        , siteName = "elm-pages"
+        , siteName = "Ctrl-R Smoothies"
         , image =
-            { url = Pages.Url.external "TODO"
-            , alt = "elm-pages logo"
+            { url = Pages.Url.external "https://images.unsplash.com/photo-1615478503562-ec2d8aa0e24e?ixlib=rb-1.2.1&raw_url=true&q=80&fm=jpg&crop=entropy&cs=tinysrgb&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887"
+            , alt = "Ctrl-R Smoothies Logo"
             , dimensions = Nothing
             , mimeType = Nothing
             }
