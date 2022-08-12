5634b8788b9ccf4d07f3ccabbd085151924b05f0
Dillon Kearns <dillon@incrementalelm.com>
2022-08-12 07:56:01 -0700

    Copy over starting point from Evan's TodoMVC app.

--- examples/todos/app/Route/Index.elm
+++ examples/todos/app/Route/Index.elm
@@ -1,13 +1,20 @@
 module Route.Index exposing (ActionData, Data, Model, Msg, route)
 
 import Api.Scalar exposing (Uuid(..))
+import Browser
+import Browser.Dom as Dom
 import DataSource exposing (DataSource)
 import Effect exposing (Effect)
 import ErrorPage exposing (ErrorPage)
 import Form
 import Form.Validation as Validation
 import Head
-import Html exposing (Html)
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Html.Keyed as Keyed
+import Html.Lazy exposing (lazy, lazy2)
+import Json.Decode as Json
 import Pages.Msg
 import Pages.PageUrl exposing (PageUrl)
 import Path exposing (Path)
@@ -16,15 +23,18 @@ import Seo.Common
 import Server.Request as Request
 import Server.Response as Response exposing (Response)
 import Shared
+import Task
 import View exposing (View)
 
 
 type alias Model =
-    {}
+    { visibility : String
+    }
 
 
 type Msg
     = NoOp
+    | ChangeVisibility String
 
 
 type alias RouteParams =
@@ -32,7 +42,8 @@ type alias RouteParams =
 
 
 type alias Data =
-    {}
+    { entries : List Entry
+    }
 
 
 type alias ActionData =
@@ -60,7 +71,10 @@ init :
     -> StaticPayload Data ActionData RouteParams
     -> ( Model, Effect Msg )
 init maybePageUrl sharedModel static =
-    ( {}, Effect.none )
+    ( { visibility = "All"
+      }
+    , Effect.none
+    )
 
 
 update :
@@ -74,6 +88,11 @@ update pageUrl sharedModel static msg model =
     case msg of
         NoOp ->
             ( model, Effect.none )
+
+        ChangeVisibility visibility ->
+            ( { model | visibility = visibility }
+            , Effect.none
+            )
 
 
 subscriptions : Maybe PageUrl -> RouteParams -> Path -> Shared.Model -> Model -> Sub Msg
@@ -90,24 +109,24 @@ head static =
 
 data : RouteParams -> Request.Parser (DataSource (Response Data ErrorPage))
 data routeParams =
-    Request.succeed (DataSource.succeed (Response.render {}))
-
-
-type Action
-    = Signout
-    | SetQuantity Uuid Int
-
-
-signoutForm : Form.HtmlForm String Action input Msg
-signoutForm =
-    Form.init
-        { combine = Validation.succeed Signout
-        , view =
-            \formState ->
-                [ Html.button [] [ Html.text "Sign out" ]
-                ]
-        }
-        |> Form.hiddenKind ( "kind", "signout" ) "Expected signout"
+    Request.succeed
+        (DataSource.succeed
+            (Response.render
+                { entries =
+                    [ { description = "Test"
+                      , completed = False
+                      , editing = False
+                      , id = 1
+                      }
+                    , { description = "Test 2"
+                      , completed = False
+                      , editing = False
+                      , id = 2
+                      }
+                    ]
+                }
+            )
+        )
 
 
 action : RouteParams -> Request.Parser (DataSource (Response ActionData ErrorPage))
@@ -122,7 +141,237 @@ view :
     -> StaticPayload Data ActionData RouteParams
     -> View (Pages.Msg.Msg Msg)
 view maybeUrl sharedModel model app =
-    { title = "Ctrl-R Smoothies"
+    { title = "Elm • TodoMVC"
     , body =
-        []
+        [ div
+            [ class "todomvc-wrapper"
+            , style "visibility" "hidden"
+            ]
+            [ section
+                [ class "todoapp" ]
+                [ --lazy viewInput model.field
+                  lazy2 viewEntries model.visibility app.data.entries
+                , lazy2 viewControls model.visibility app.data.entries
+                ]
+            , infoFooter
+            ]
+        ]
     }
+
+
+type alias Entry =
+    { description : String
+    , completed : Bool
+    , editing : Bool
+    , id : Int
+    }
+
+
+
+-- VIEW
+
+
+viewInput : String -> Html Msg
+viewInput task =
+    header
+        [ class "header" ]
+        [ h1 [] [ text "todos" ]
+        , input
+            [ class "new-todo"
+            , placeholder "What needs to be done?"
+            , autofocus True
+            , value task
+            , name "newTodo"
+            ]
+            []
+        ]
+
+
+
+-- VIEW ALL ENTRIES
+
+
+viewEntries : String -> List Entry -> Html (Pages.Msg.Msg Msg)
+viewEntries visibility entries =
+    let
+        isVisible todo =
+            case visibility of
+                "Completed" ->
+                    todo.completed
+
+                "Active" ->
+                    not todo.completed
+
+                _ ->
+                    True
+
+        allCompleted =
+            List.all .completed entries
+
+        cssVisibility =
+            if List.isEmpty entries then
+                "hidden"
+
+            else
+                "visible"
+    in
+    section
+        [ class "main"
+        , style "visibility" cssVisibility
+        ]
+        [ input
+            [ class "toggle-all"
+            , type_ "checkbox"
+            , name "toggle"
+            , checked allCompleted
+
+            --, onClick (CheckAll (not allCompleted))
+            ]
+            []
+        , label
+            [ for "toggle-all" ]
+            [ text "Mark all as complete" ]
+        , Keyed.ul [ class "todo-list" ] <|
+            List.map viewKeyedEntry (List.filter isVisible entries)
+        ]
+
+
+
+-- VIEW INDIVIDUAL ENTRIES
+
+
+viewKeyedEntry : Entry -> ( String, Html (Pages.Msg.Msg Msg) )
+viewKeyedEntry todo =
+    ( String.fromInt todo.id, lazy viewEntry todo )
+
+
+viewEntry : Entry -> Html (Pages.Msg.Msg Msg)
+viewEntry todo =
+    li
+        [ classList [ ( "completed", todo.completed ), ( "editing", todo.editing ) ] ]
+        [ div
+            [ class "view" ]
+            [ input
+                [ class "toggle"
+                , type_ "checkbox"
+                , checked todo.completed
+
+                --, onClick (Check todo.id (not todo.completed))
+                ]
+                []
+            , label
+                [--onDoubleClick (EditingEntry todo.id True)
+                ]
+                [ text todo.description ]
+            , button
+                [ class "destroy"
+
+                --, onClick (Delete todo.id)
+                ]
+                []
+            ]
+        , input
+            [ class "edit"
+            , value todo.description
+            , name "title"
+            , id ("todo-" ++ String.fromInt todo.id)
+
+            --, onInput (UpdateEntry todo.id)
+            --, onBlur (EditingEntry todo.id False)
+            --, onEnter (EditingEntry todo.id False)
+            ]
+            []
+        ]
+
+
+
+-- VIEW CONTROLS AND FOOTER
+
+
+viewControls : String -> List Entry -> Html (Pages.Msg.Msg Msg)
+viewControls visibility entries =
+    let
+        entriesCompleted =
+            List.length (List.filter .completed entries)
+
+        entriesLeft =
+            List.length entries - entriesCompleted
+    in
+    footer
+        [ class "footer"
+        , hidden (List.isEmpty entries)
+        ]
+        [ lazy viewControlsCount entriesLeft
+        , lazy viewControlsFilters visibility
+        , lazy viewControlsClear entriesCompleted
+        ]
+
+
+viewControlsCount : Int -> Html (Pages.Msg.Msg Msg)
+viewControlsCount entriesLeft =
+    let
+        item_ =
+            if entriesLeft == 1 then
+                " item"
+
+            else
+                " items"
+    in
+    span
+        [ class "todo-count" ]
+        [ strong [] [ text (String.fromInt entriesLeft) ]
+        , text (item_ ++ " left")
+        ]
+
+
+viewControlsFilters : String -> Html (Pages.Msg.Msg Msg)
+viewControlsFilters visibility =
+    ul
+        [ class "filters" ]
+        [ visibilitySwap "#/" "All" visibility
+        , text " "
+        , visibilitySwap "#/active" "Active" visibility
+        , text " "
+        , visibilitySwap "#/completed" "Completed" visibility
+        ]
+
+
+visibilitySwap : String -> String -> String -> Html (Pages.Msg.Msg Msg)
+visibilitySwap uri visibility actualVisibility =
+    li
+        [--onClick (ChangeVisibility visibility)
+        ]
+        [ a [ href uri, classList [ ( "selected", visibility == actualVisibility ) ] ]
+            [ text visibility ]
+        ]
+
+
+viewControlsClear : Int -> Html (Pages.Msg.Msg Msg)
+viewControlsClear entriesCompleted =
+    button
+        [ class "clear-completed"
+        , hidden (entriesCompleted == 0)
+
+        --, onClick DeleteComplete
+        ]
+        [ text ("Clear completed (" ++ String.fromInt entriesCompleted ++ ")")
+        ]
+
+
+infoFooter : Html msg
+infoFooter =
+    footer [ class "info" ]
+        [ p [] [ text "Double-click to edit a todo" ]
+        , p []
+            [ text "Written by "
+            , a [ href "https://github.com/dillonkearns" ] [ text "Dillon Kearns" ]
+            ]
+        , p []
+            [ text "Forked from Evan Czaplicki's vanilla Elm implementation "
+            , a [ href "https://github.com/evancz/elm-todomvc/blob/f236e7e56941c7705aba6e42cb020ff515fe3290/src/Main.elm" ] [ text "github.com/evancz/elm-todomvc" ]
+            ]
+        , p []
+            [ text "Part of "
+            , a [ href "http://todomvc.com" ] [ text "TodoMVC" ]
+            ]
+        ]
