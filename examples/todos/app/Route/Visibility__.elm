725675a94fa101b7c0eebec28160c24961d1d0b2
Dillon Kearns <dillon@incrementalelm.com>
2022-08-16 15:51:12 -0700

    Use helper function.

--- examples/todos/app/Route/Visibility__.elm
+++ examples/todos/app/Route/Visibility__.elm
@@ -176,47 +176,16 @@ action routeParams =
                         )
 
                 Ok (UpdateEntry ( itemId, newDescription )) ->
-                    okSessionThing
-                        |> Session.get "sessionId"
-                        |> Maybe.map Data.Session.get
-                        |> Maybe.map Request.Hasura.dataSource
-                        |> Maybe.map
-                            (DataSource.andThen
-                                (\maybeUserSession ->
-                                    let
-                                        bar : Maybe Uuid
-                                        bar =
-                                            maybeUserSession
-                                                |> Maybe.map .id
-                                    in
-                                    case bar of
-                                        Nothing ->
-                                            DataSource.succeed
-                                                ( okSessionThing
-                                                , Response.render {}
-                                                )
-
-                                        Just userId ->
-                                            Data.Todo.update
-                                                { userId = userId
-                                                , todoId = Uuid itemId
-                                                , newDescription = newDescription
-                                                }
-                                                |> Request.Hasura.mutationDataSource
-                                                |> DataSource.map
-                                                    (\() ->
-                                                        ( okSessionThing
-                                                        , Response.render {}
-                                                        )
-                                                    )
-                                )
-                            )
-                        |> Maybe.withDefault
-                            (DataSource.succeed
-                                ( okSessionThing
-                                , Response.render {}
-                                )
-                            )
+                    withUserSession session
+                        (\userId ->
+                            Data.Todo.update
+                                { userId = userId
+                                , todoId = Uuid itemId
+                                , newDescription = newDescription
+                                }
+                                |> Request.Hasura.mutationDataSource
+                                |> DataSource.map (\() -> Response.render {})
+                        )
 
                 Ok (Delete itemId) ->
                     okSessionThing
