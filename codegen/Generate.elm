012aac229a9710ec0ef7db75863aea39b6f31163
Dillon Kearns <dillon@incrementalelm.com>
2022-09-12 15:01:59 -0700

    Add initial function generation for segmentsToRoute.

--- codegen/Generate.elm
+++ codegen/Generate.elm
@@ -4,6 +4,7 @@ port module Generate exposing (main)
 
 import Elm exposing (File)
 import Elm.Annotation
+import Elm.Case
 import Elm.Op
 import Gen.Basics
 import Gen.CodeGen.Generate exposing (Error)
@@ -11,7 +12,7 @@ import Gen.List
 import Gen.Path
 import Gen.Server.Response
 import Gen.String
-import Pages.Internal.RoutePattern as RoutePattern
+import Pages.Internal.RoutePattern as RoutePattern exposing (RoutePattern)
 
 
 type alias Flags =
@@ -46,6 +47,36 @@ file templates =
         [ "Route" ]
         [ Elm.customType "Route"
             (routes |> List.map RoutePattern.toVariant)
+            |> expose
+        , Elm.declaration "segmentsToRoute"
+            (Elm.fn
+                ( "segments"
+                , Elm.Annotation.list Elm.Annotation.string |> Just
+                )
+                (\segments ->
+                    Elm.Case.custom segments
+                        (Elm.Annotation.list Elm.Annotation.string)
+                        ((routes |> List.map routeToBranch)
+                            ++ [ Elm.Case.otherwise (\_ -> Elm.nothing) ]
+                        )
+                        |> Elm.withType
+                            (Elm.Annotation.named [] "Route"
+                                |> Elm.Annotation.maybe
+                            )
+                )
+            )
+            |> expose
+        , Elm.declaration "urlToRoute"
+            (Elm.fn
+                ( "url"
+                , Elm.Annotation.extensible "url" [ ( "path", Elm.Annotation.string ) ]
+                    |> Just
+                )
+                (\url ->
+                    url
+                        |> Elm.get "path"
+                )
+            )
             |> expose
         , Elm.declaration "baseUrl" (Elm.string "/")
             |> expose
@@ -95,6 +126,15 @@ file templates =
             )
             |> expose
         ]
+
+
+routeToBranch : RoutePattern -> Elm.Case.Branch
+routeToBranch route =
+    Elm.Case.branchList 0
+        (\_ ->
+            Elm.val "Index"
+                |> Elm.just
+        )
 
 
 expose : Elm.Declaration -> Elm.Declaration
