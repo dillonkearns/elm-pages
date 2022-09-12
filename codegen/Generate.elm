2e881e0de5eb45eb04684afca31ca7035d68fd18
Dillon Kearns <dillon@incrementalelm.com>
2022-09-12 14:36:54 -0700

    Add more functions to generated Route.elm.

--- codegen/Generate.elm
+++ codegen/Generate.elm
@@ -3,7 +3,14 @@ port module Generate exposing (main)
 {-| -}
 
 import Elm exposing (File)
+import Elm.Annotation
+import Elm.Op
+import Gen.Basics
 import Gen.CodeGen.Generate exposing (Error)
+import Gen.List
+import Gen.Path
+import Gen.Server.Response
+import Gen.String
 import Pages.Internal.RoutePattern as RoutePattern
 
 
@@ -35,11 +42,57 @@ file templates =
             templates
                 |> List.filterMap RoutePattern.fromModuleName
     in
-    Elm.file [ "Route" ]
+    Elm.file
+        [ "Route" ]
         [ Elm.customType "Route"
             (routes |> List.map RoutePattern.toVariant)
             |> expose
         , Elm.declaration "baseUrl" (Elm.string "/")
+            |> expose
+        , Elm.declaration "baseUrlAsPath"
+            (Gen.List.call_.filter
+                (Elm.fn ( "item", Nothing )
+                    (\item ->
+                        Gen.Basics.call_.not
+                            (Gen.String.call_.isEmpty item)
+                    )
+                )
+                (Gen.String.call_.split (Elm.string "/")
+                    (Elm.val "baseUrl")
+                )
+            )
+            |> expose
+        , Elm.declaration "toPath"
+            (Elm.fn ( "route", Elm.Annotation.named [] "Route" |> Just )
+                (\route ->
+                    Gen.Path.call_.fromString
+                        (Gen.String.call_.join
+                            (Elm.string "/")
+                            (Elm.Op.append
+                                (Elm.val "baseUrlAsPath")
+                                (Elm.apply (Elm.val "routeToPath")
+                                    [ route ]
+                                )
+                            )
+                        )
+                )
+            )
+            |> expose
+        , Elm.declaration "toString"
+            (Elm.fn ( "route", Elm.Annotation.named [] "Route" |> Just )
+                (\route ->
+                    Gen.Path.toAbsolute
+                        (Elm.apply (Elm.val "toPath") [ route ])
+                )
+            )
+            |> expose
+        , Elm.declaration "redirectTo"
+            (Elm.fn ( "route", Elm.Annotation.named [] "Route" |> Just )
+                (\route ->
+                    Gen.Server.Response.call_.temporaryRedirect
+                        route
+                )
+            )
             |> expose
         ]
 
