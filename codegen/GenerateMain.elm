2801ec542a11d09ab3b1865914e7cec7d7e1e026
Dillon Kearns <dillon@incrementalelm.com>
2022-09-15 11:11:05 -0700

    Build up more generated Main.elm code.

--- codegen/GenerateMain.elm
+++ codegen/GenerateMain.elm
@@ -16,6 +16,7 @@ import Gen.Path
 import Gen.Server.Response
 import Gen.String
 import Gen.Tuple
+import Gen.Url
 import Pages.Internal.RoutePattern as RoutePattern exposing (RoutePattern)
 import Pretty
 import Regex exposing (Regex)
@@ -60,4 +61,76 @@ otherFile routes =
                    , Elm.variant "NotFound"
                    ]
             )
+        , Elm.customType "Msg"
+            ((routes
+                |> List.map
+                    (\route ->
+                        Elm.variantWith
+                            ("Msg"
+                                ++ (RoutePattern.toModuleName route |> String.join "__")
+                            )
+                            [ Type.named
+                                ("Route"
+                                    :: RoutePattern.toModuleName route
+                                )
+                                "Msg"
+                            ]
+                    )
+             )
+                ++ [ Elm.variantWith "MsgGlobal" [ Type.named [ "Shared" ] "Msg" ]
+                   , Elm.variantWith "OnPageChange"
+                        [ Type.record
+                            [ ( "protocol", Gen.Url.annotation_.protocol )
+                            , ( "host", Type.string )
+                            , ( "port_", Type.maybe Type.int )
+                            , ( "path", pathType )
+                            , ( "query", Type.maybe Type.string )
+                            , ( "fragment", Type.maybe Type.string )
+                            , ( "metadata", Type.maybe (Type.named [ "Route" ] "Route") )
+                            ]
+                        ]
+                   , Elm.variantWith "MsgErrorPage____" [ Type.named [ "ErrorPage" ] "Msg" ]
+                   ]
+            )
+        , Elm.customType "PageData"
+            ((routes
+                |> List.map
+                    (\route ->
+                        Elm.variantWith
+                            ("Data"
+                                ++ (RoutePattern.toModuleName route |> String.join "__")
+                            )
+                            [ Type.named
+                                ("Route"
+                                    :: RoutePattern.toModuleName route
+                                )
+                                "Data"
+                            ]
+                    )
+             )
+                ++ [ Elm.variant "Data404NotFoundPage____"
+                   , Elm.variantWith "DataErrorPage____" [ Type.named [ "ErrorPage" ] "ErrorPage" ]
+                   ]
+            )
+        , Elm.customType "ActionData"
+            (routes
+                |> List.map
+                    (\route ->
+                        Elm.variantWith
+                            ("ActionData"
+                                ++ (RoutePattern.toModuleName route |> String.join "__")
+                            )
+                            [ Type.named
+                                ("Route"
+                                    :: RoutePattern.toModuleName route
+                                )
+                                "ActionData"
+                            ]
+                    )
+            )
         ]
+
+
+pathType : Type.Annotation
+pathType =
+    Type.named [ "Path" ] "Path"
