96dd80e8a083991b6f7face2c2e45c502cf7913e
Dillon Kearns <dillon@incrementalelm.com>
2022-10-04 13:04:13 -0700

    Move elm-review dependencies out into hidden review sub-projects.

--- test.sh
+++ test.sh
@@ -3,6 +3,8 @@ root=`pwd`
 yes | lamdera reset || true
 elm-test-rs --compiler=lamdera
 cd examples/routing && npm i && npm run build && elm-test-rs --compiler=lamdera && cd $root
+(cd generator/dead-code-review && elm-test-rs --compiler=lamdera)
+(cd generator/review && elm-test-rs --compiler=lamdera)
 npm run test:snapshot
 npx elmi-to-json --version
 elm-verify-examples --run-tests --elm-test-args '--compiler=lamdera'
