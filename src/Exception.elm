ad42776fb24c721b62b15b521c6e95c1d15c3c5a
Dillon Kearns <dillon@incrementalelm.com>
2022-12-28 19:29:08 -0700

    Update docs.

--- src/Exception.elm
+++ src/Exception.elm
@@ -1,24 +1,35 @@
-module Exception exposing (Catchable(..), Throwable, fromString, fromStringWithValue, throw)
+module Exception exposing (Throwable, Catchable(..), fromString, fromStringWithValue, throw)
+
+{-|
+
+@docs Throwable, Catchable, fromString, fromStringWithValue, throw
+
+-}
 
 
+{-| -}
 type alias Throwable =
     Catchable ()
 
 
+{-| -}
 type Catchable error
     = Catchable error String
 
 
+{-| -}
 fromString : String -> Catchable ()
 fromString string =
     Catchable () string
 
 
+{-| -}
 fromStringWithValue : String -> value -> Catchable value
 fromStringWithValue string value =
     Catchable value string
 
 
+{-| -}
 throw : Catchable error -> Catchable ()
 throw exception =
     case exception of
