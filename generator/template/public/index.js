dd365af51d578c60d24ea5f43de25e62ef04c1da
EvanPiro <dev.evanpiro@gmail.com>
2023-01-09 10:54:29 -0500

    remove console log from template

--- generator/template/public/index.js
+++ generator/template/public/index.js
@@ -3,8 +3,7 @@
 /** @type ElmPagesInit */
 export default {
   load: async function (elmLoaded) {
-    const app = await elmLoaded;
-    console.log("App loaded", app);
+    await elmLoaded;
   },
   flags: function () {
     return "You can decode this in Shared.elm using Json.Decode.string!";
