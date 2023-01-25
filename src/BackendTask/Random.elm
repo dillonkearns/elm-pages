b989ba624e4f5c900ca251c943501f9cc465d9d8
Dillon Kearns <dillon@incrementalelm.com>
2023-01-25 15:16:04 -0800

    Update docs.

--- src/BackendTask/Random.elm
+++ src/BackendTask/Random.elm
@@ -33,6 +33,13 @@ The random initial seed is generated using <https://developer.mozilla.org/en-US/
 to generate a single 32-bit Integer. That 32-bit Integer is then used with `Random.initialSeed` to create an Elm Random.Seed value.
 Then that `Seed` used to run the `Generator`.
 
+Note that this is different than `elm/random`'s `Random.generate`. This difference shouldn't be problematic, and in fact the `BackendTask`
+random seed generation is more suitable for cryptographically secure random generation because you can't determine the
+random seed based solely on the time at which it is run. Each time you call `BackendTask.generate` it uses a newly
+generated random seed to run the `Random.Generator` that is passed in. In contrast, `elm/random`'s `Random.generate`
+generates an initial seed using `Time.now`, and then continues with that same seed using using [`Random.step`](https://package.elm-lang.org/packages/elm/random/latest/Random#step)
+to get new random values after that. You can [see the implementation here](https://github.com/elm/random/blob/c1c9da4d861363cee1c93382d2687880279ed0dd/src/Random.elm#L865-L896).
+
 -}
 generate : Random.Generator value -> BackendTask error value
 generate generator =
