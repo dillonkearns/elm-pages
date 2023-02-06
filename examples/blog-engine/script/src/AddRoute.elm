21e38074c5c2c705ec61ee2cb296290421d115fc
Dillon Kearns <dillon@incrementalelm.com>
2023-02-06 13:55:17 -0800

    Rename AddForm to AddRoute now that they handle both cases.

--- examples/blog-engine/script/src/AddRoute.elm
+++ examples/blog-engine/script/src/AddRoute.elm
@@ -1,22 +1,38 @@
 module AddRoute exposing (run)
 
+import AddFormHelp
 import BackendTask
 import Cli.Option as Option
 import Cli.OptionsParser as OptionsParser
 import Cli.Program as Program
-import Cli.Validate
 import Elm
 import Elm.Annotation
 import Elm.Case
+import Elm.Declare
+import Elm.Let
+import Elm.Op
 import Gen.BackendTask
+import Gen.Debug
 import Gen.Effect
-import Gen.Html
+import Gen.Form
+import Gen.Form.FieldView
+import Gen.Html as Html
+import Gen.Html.Attributes
+import Gen.List
+import Gen.Pages.Script
 import Gen.Platform.Sub
 import Gen.Server.Request
 import Gen.Server.Response
 import Gen.View
+import List.Extra
 import Pages.Generate exposing (Type(..))
 import Pages.Script as Script exposing (Script)
+
+
+type alias CliOptions =
+    { moduleName : String
+    , rest : List ( String, AddFormHelp.Kind )
+    }
 
 
 run : Script
@@ -26,7 +42,7 @@ run =
             let
                 file : Elm.File
                 file =
-                    buildFile (cliOptions.moduleName |> String.split ".")
+                    createFile (cliOptions.moduleName |> String.split ".") cliOptions.rest
             in
             Script.writeFile
                 { path = "app/" ++ file.path
@@ -36,41 +52,119 @@ run =
         )
 
 
-type alias CliOptions =
-    { moduleName : String
-    }
-
-
 program : Program.Config CliOptions
 program =
     Program.config
         |> Program.add
             (OptionsParser.build CliOptions
-                |> OptionsParser.with
-                    (Option.requiredPositionalArg "module"
-                        |> Option.validate (Cli.Validate.regex moduleNameRegex)
-                    )
+                |> OptionsParser.with (Option.requiredPositionalArg "module" |> Pages.Generate.moduleNameCliArg)
+                |> OptionsParser.withRestArgs AddFormHelp.restArgsParser
             )
 
 
-moduleNameRegex : String
-moduleNameRegex =
-    "^[A-Z][a-zA-Z0-9_]*(\\.([A-Z][a-zA-Z0-9_]*))*$"
-
-
-buildFile : List String -> Elm.File
-buildFile moduleName =
+createFile : List String -> List ( String, AddFormHelp.Kind ) -> Elm.File
+createFile moduleName fields =
+    let
+        formHelpers :
+            Maybe
+                { formHandlers : { declaration : Elm.Declaration, value : Elm.Expression }
+                , renderForm : Elm.Expression -> Elm.Expression
+                , declarations : List Elm.Declaration
+                }
+        formHelpers =
+            AddFormHelp.provide
+                { fields = fields
+                , view =
+                    \{ formState, params } ->
+                        Elm.Let.letIn
+                            (\fieldView ->
+                                Elm.list
+                                    ((params
+                                        |> List.Extra.zip fields
+                                        |> List.map
+                                            (\( ( name, kind ), param ) ->
+                                                fieldView (Elm.string name) param
+                                            )
+                                     )
+                                        ++ [ Elm.ifThen (formState |> Elm.get "isTransitioning")
+                                                (Html.button
+                                                    [ Gen.Html.Attributes.disabled True
+                                                    ]
+                                                    [ Html.text "Submitting..."
+                                                    ]
+                                                )
+                                                (Html.button []
+                                                    [ Html.text "Submit"
+                                                    ]
+                                                )
+                                           ]
+                                    )
+                            )
+                            |> Elm.Let.fn2 "fieldView"
+                                ( "label", Elm.Annotation.string |> Just )
+                                ( "field", Nothing )
+                                (\label field ->
+                                    Html.div []
+                                        [ Html.label []
+                                            [ Html.call_.text (Elm.Op.append label (Elm.string " "))
+                                            , field |> Gen.Form.FieldView.input []
+                                            , errorsView.call (Elm.get "errors" formState) field
+                                            ]
+                                        ]
+                                )
+                            |> Elm.Let.toExpression
+                }
+    in
     Pages.Generate.serverRender
         { moduleName = moduleName
         , action =
-            ( Alias (Elm.Annotation.record [])
-            , \routeParams ->
-                Gen.Server.Request.succeed
-                    (Gen.BackendTask.succeed
-                        (Gen.Server.Response.render
-                            (Elm.record [])
-                        )
+            ( Alias
+                (Elm.Annotation.record
+                    (case formHelpers of
+                        Just _ ->
+                            [ ( "errors", Elm.Annotation.namedWith [ "Form" ] "Response" [ Elm.Annotation.string ] )
+                            ]
+
+                        Nothing ->
+                            []
                     )
+                )
+            , \routeParams ->
+                formHelpers
+                    |> Maybe.map
+                        (\justFormHelp ->
+                            Gen.Server.Request.formData justFormHelp.formHandlers.value
+                                |> Gen.Server.Request.call_.map
+                                    (Elm.fn ( "formData", Nothing )
+                                        (\formData ->
+                                            Elm.Case.tuple formData
+                                                "response"
+                                                "parsedForm"
+                                                (\response parsedForm ->
+                                                    Gen.Debug.toString parsedForm
+                                                        |> Gen.Pages.Script.call_.log
+                                                        |> Gen.BackendTask.call_.map
+                                                            (Elm.fn ( "_", Nothing )
+                                                                (\_ ->
+                                                                    Gen.Server.Response.render
+                                                                        (Elm.record
+                                                                            [ ( "errors", response )
+                                                                            ]
+                                                                        )
+                                                                )
+                                                            )
+                                                )
+                                        )
+                                    )
+                        )
+                    |> Maybe.withDefault
+                        (Gen.Server.Request.succeed
+                            (Gen.BackendTask.succeed
+                                (Gen.Server.Response.render
+                                    (Elm.record [])
+                                )
+                            )
+                        )
             )
         , data =
             ( Alias (Elm.Annotation.record [])
@@ -84,12 +178,29 @@ buildFile moduleName =
             )
         , head = \app -> Elm.list []
         }
+        |> Pages.Generate.addDeclarations
+            (formHelpers
+                |> Maybe.map .declarations
+                |> Maybe.map ((::) errorsView.declaration)
+                |> Maybe.withDefault []
+            )
         |> Pages.Generate.buildWithLocalState
             { view =
                 \{ maybeUrl, sharedModel, model, app } ->
                     Gen.View.make_.view
                         { title = moduleName |> String.join "." |> Elm.string
-                        , body = Elm.list [ Gen.Html.text "Here is your generated page!!!" ]
+                        , body =
+                            Elm.list
+                                (case formHelpers of
+                                    Just justFormHelp ->
+                                        [ Html.h2 [] [ Html.text "Form" ]
+                                        , justFormHelp.renderForm app -- TODO customize argument with `(Elm.get "errors" >> Elm.just)` and `Elm.unit`?
+                                        ]
+
+                                    Nothing ->
+                                        [ Html.h2 [] [ Html.text "New Page" ]
+                                        ]
+                                )
                         }
             , update =
                 \{ pageUrl, sharedModel, app, msg, model } ->
@@ -116,6 +227,56 @@ buildFile moduleName =
             , msg =
                 Custom [ Elm.variant "NoOp" ]
             }
+
+
+errorsView :
+    { declaration : Elm.Declaration
+    , call : Elm.Expression -> Elm.Expression -> Elm.Expression
+    , callFrom : List String -> Elm.Expression -> Elm.Expression -> Elm.Expression
+    }
+errorsView =
+    Elm.Declare.fn2 "errorsView"
+        ( "errors", Elm.Annotation.namedWith [ "Form" ] "Errors" [ Elm.Annotation.string ] |> Just )
+        ( "field"
+        , Elm.Annotation.namedWith [ "Form", "Validation" ]
+            "Field"
+            [ Elm.Annotation.string
+            , Elm.Annotation.var "parsed"
+            , Elm.Annotation.var "kind"
+            ]
+            |> Just
+        )
+        (\errors field ->
+            Elm.ifThen
+                (Gen.List.call_.isEmpty (Gen.Form.errorsForField field errors))
+                (Html.div [] [])
+                (Html.div
+                    []
+                    [ Html.call_.ul (Elm.list [])
+                        (Gen.List.call_.map
+                            (Elm.fn ( "error", Nothing )
+                                (\error ->
+                                    Html.li
+                                        [ Gen.Html.Attributes.style "color" "red"
+                                        ]
+                                        [ Html.call_.text error
+                                        ]
+                                )
+                            )
+                            (Gen.Form.errorsForField field errors)
+                        )
+                    ]
+                )
+                |> Elm.withType
+                    (Elm.Annotation.namedWith [ "Html" ]
+                        "Html"
+                        [ Elm.Annotation.namedWith
+                            [ "Pages", "Msg" ]
+                            "Msg"
+                            [ Elm.Annotation.named [] "Msg" ]
+                        ]
+                    )
+        )
 
 
 effectType : Elm.Annotation.Annotation
