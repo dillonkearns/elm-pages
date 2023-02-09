82974732210d683229d990e6fb9951996e817117
Dillon Kearns <dillon@incrementalelm.com>
2023-02-09 12:05:02 -0800

    Update compatability keys and NPM version.

--- generator/src/compatibility-key.js
+++ generator/src/compatibility-key.js
@@ -1,3 +1,3 @@
-export const compatibilityKey = 7;
+export const compatibilityKey = 8;
 
-export const packageVersion = "3.0.0-beta.22";
+export const packageVersion = "3.0.0-beta.23";
--- update-compatibility-keys.js
+++ update-compatibility-keys.js
@@ -2,7 +2,7 @@
 
 import * as fs from "node:fs";
 
-const currentCompatibilityKey = 7;
+const currentCompatibilityKey = 8;
 const currentPackageVersion = JSON.parse(
   fs.readFileSync("./package.json")
 ).version;
