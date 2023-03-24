938f0c85e63b5fb78c72d136200fe66bd2597404
Dillon Kearns <dillon@incrementalelm.com>
2023-03-24 12:10:41 -0700

    Update test script.

--- test.sh
+++ test.sh
@@ -1,9 +1,9 @@
 set -ex;
 yes | lamdera reset || true
-npx elm-test-rs --compiler=lamdera
-(cd examples/routing && npm i && npm run build && npx elm-test-rs --compiler=lamdera)
-(cd generator/dead-code-review && npx elm-test-rs --compiler=lamdera)
-(cd generator/review && npx elm-test-rs --compiler=lamdera)
+npx elm-test-rs
+(cd examples/routing && npm i && npm run build && npx elm-test-rs)
+(cd generator/dead-code-review && npx elm-test-rs)
+(cd generator/review && npx elm-test-rs)
 npm run test:snapshot
 npx elmi-to-json --version
 elm-verify-examples --run-tests --elm-test-args '--compiler=lamdera'
