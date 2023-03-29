afb4c3fad104a90277d261e968666703eafac7c6
Dillon Kearns <dillon@incrementalelm.com>
2023-03-29 10:19:19 -0700

    Update compatability key.

--- generator/src/compatibility-key.js
+++ generator/src/compatibility-key.js
@@ -1,3 +1,3 @@
-export const compatibilityKey = 12;
+export const compatibilityKey = 13;
 
 export const packageVersion = "3.0.0-beta.32";
--- update-compatibility-keys.js
+++ update-compatibility-keys.js
@@ -2,7 +2,7 @@
 
 import * as fs from "node:fs";
 
-const currentCompatibilityKey = 12;
+const currentCompatibilityKey = 13;
 const currentPackageVersion = JSON.parse(
   fs.readFileSync("./package.json")
 ).version;
