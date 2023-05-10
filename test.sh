24e4587221576ad413ca64466c99e19eaba05a54
Dillon Kearns <dillon@incrementalelm.com>
2023-05-10 10:22:25 -0700

    Add a reset to CI to fix error.

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 set -ex;
 yes | lamdera reset || true
 npx elm-test-rs
-(cd examples/routing && npm i && npm run build && npx elm-test-rs)
+(cd examples/routing && yes | lamdera reset || true && npm i && npm run build && npx elm-test-rs)
 (cd generator/dead-code-review && npx elm-test-rs)
 (cd generator/review && npx elm-test-rs)
 npm run test:snapshot
