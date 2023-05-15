55587535cd345b9f66873cb7f8e6d88ea9413d49
Dillon Kearns <dillon@incrementalelm.com>
2023-05-15 08:48:36 -0700

    Formatting.

--- generator/src/build.js
+++ generator/src/build.js
@@ -195,10 +195,13 @@ export async function run(options) {
 import * as renderer from "./render.js";
 import * as elmModule from "${path.resolve("./elm-stuff/elm-pages/elm.cjs")}";
 import * as url from 'url';
-${global.portsFilePath
-          ? `import * as customBackendTask from "${path.resolve(global.portsFilePath)}";`
-          : `const customBackendTask = {};`
-        }
+${
+  global.portsFilePath
+    ? `import * as customBackendTask from "${path.resolve(
+        global.portsFilePath
+      )}";`
+    : `const customBackendTask = {};`
+}
 
 import * as preRenderHtml from "./pre-render-html.js";
 const basePath = \`${options.base || "/"}\`;
