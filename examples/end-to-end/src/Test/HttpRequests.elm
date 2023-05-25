556302d75da5ab007a7e70dff4c7a3066ef3803c
Dillon Kearns <dillon@incrementalelm.com>
2023-05-25 09:18:20 -0700

    Try commenting out tests with GitHub API requests to see if rate limiting fixes CI.

--- examples/end-to-end/src/Test/HttpRequests.elm
+++ examples/end-to-end/src/Test/HttpRequests.elm
@@ -44,74 +44,75 @@ all =
                     Ok () ->
                         Expect.fail "Expected HTTP error, got Ok"
             )
-    , BackendTask.Http.getJson "https://api.github.com/repos/dillonkearns/elm-pages" (Decode.field "stargazers_count" Decode.int)
-        |> BackendTask.mapError .recoverable
-        |> test "200 JSON"
-            (\result ->
-                case result of
-                    Err error ->
-                        Expect.fail ("Expected BadStatus, got: " ++ Debug.toString error)
 
-                    Ok count ->
-                        Expect.pass
-            )
-    , BackendTask.Http.getJson "https://api.github.com/repos/dillonkearns/elm-pages" (Decode.field "this-field-doesn't-exist" Decode.int)
-        |> BackendTask.mapError .recoverable
-        |> test "JSON decoding error"
-            (\result ->
-                case result of
-                    Err (BackendTask.Http.BadBody (Just (Decode.Failure failureString _)) _) ->
-                        failureString
-                            |> Expect.equal "Expecting an OBJECT with a field named `this-field-doesn't-exist`"
-
-                    _ ->
-                        Expect.fail ("Expected BadStatus, got: " ++ Debug.toString result)
-            )
-    , BackendTask.Http.getWithOptions
-        { url = "https://api.github.com/repos/dillonkearns/elm-pages"
-        , headers = []
-        , cacheStrategy = Just BackendTask.Http.IgnoreCache
-        , cachePath = Nothing
-        , retries = Nothing
-        , timeoutInMs = Nothing
-        , expect =
-            BackendTask.Http.expectJson
-                (Decode.field "this-field-doesn't-exist" Decode.int)
-        }
-        |> BackendTask.mapError .recoverable
-        |> test "cache options"
-            (\result ->
-                case result of
-                    Err (BackendTask.Http.BadBody (Just (Decode.Failure failureString _)) _) ->
-                        failureString
-                            |> Expect.equal "Expecting an OBJECT with a field named `this-field-doesn't-exist`"
-
-                    _ ->
-                        Expect.fail ("Expected BadStatus, got: " ++ Debug.toString result)
-            )
-    , BackendTask.Http.getWithOptions
-        { url = "https://api.github.com/repos/dillonkearns/elm-pages"
-        , headers = []
-        , cacheStrategy = Just BackendTask.Http.ForceRevalidate
-        , cachePath = Nothing
-        , retries = Nothing
-        , timeoutInMs = Nothing
-        , expect =
-            BackendTask.Http.withMetadata Tuple.pair
-                (BackendTask.Http.expectJson
-                    (Decode.field "stargazers_count" Decode.int)
-                )
-        }
-        |> test "with metadata"
-            (\result ->
-                case result of
-                    Ok ( metadata, stars ) ->
-                        metadata.statusCode
-                            |> Expect.equal 200
-
-                    _ ->
-                        Expect.fail ("Expected Ok, got: " ++ Debug.toString result)
-            )
+    --, BackendTask.Http.getJson "https://api.github.com/repos/dillonkearns/elm-pages" (Decode.field "stargazers_count" Decode.int)
+    --    |> BackendTask.mapError .recoverable
+    --    |> test "200 JSON"
+    --        (\result ->
+    --            case result of
+    --                Err error ->
+    --                    Expect.fail ("Expected BadStatus, got: " ++ Debug.toString error)
+    --
+    --                Ok count ->
+    --                    Expect.pass
+    --        )
+    --, BackendTask.Http.getJson "https://api.github.com/repos/dillonkearns/elm-pages" (Decode.field "this-field-doesn't-exist" Decode.int)
+    --    |> BackendTask.mapError .recoverable
+    --    |> test "JSON decoding error"
+    --        (\result ->
+    --            case result of
+    --                Err (BackendTask.Http.BadBody (Just (Decode.Failure failureString _)) _) ->
+    --                    failureString
+    --                        |> Expect.equal "Expecting an OBJECT with a field named `this-field-doesn't-exist`"
+    --
+    --                _ ->
+    --                    Expect.fail ("Expected BadStatus, got: " ++ Debug.toString result)
+    --        )
+    --, BackendTask.Http.getWithOptions
+    --    { url = "https://api.github.com/repos/dillonkearns/elm-pages"
+    --    , headers = []
+    --    , cacheStrategy = Just BackendTask.Http.IgnoreCache
+    --    , cachePath = Nothing
+    --    , retries = Nothing
+    --    , timeoutInMs = Nothing
+    --    , expect =
+    --        BackendTask.Http.expectJson
+    --            (Decode.field "this-field-doesn't-exist" Decode.int)
+    --    }
+    --    |> BackendTask.mapError .recoverable
+    --    |> test "cache options"
+    --        (\result ->
+    --            case result of
+    --                Err (BackendTask.Http.BadBody (Just (Decode.Failure failureString _)) _) ->
+    --                    failureString
+    --                        |> Expect.equal "Expecting an OBJECT with a field named `this-field-doesn't-exist`"
+    --
+    --                _ ->
+    --                    Expect.fail ("Expected BadStatus, got: " ++ Debug.toString result)
+    --        )
+    --, BackendTask.Http.getWithOptions
+    --    { url = "https://api.github.com/repos/dillonkearns/elm-pages"
+    --    , headers = []
+    --    , cacheStrategy = Just BackendTask.Http.ForceRevalidate
+    --    , cachePath = Nothing
+    --    , retries = Nothing
+    --    , timeoutInMs = Nothing
+    --    , expect =
+    --        BackendTask.Http.withMetadata Tuple.pair
+    --            (BackendTask.Http.expectJson
+    --                (Decode.field "stargazers_count" Decode.int)
+    --            )
+    --    }
+    --    |> test "with metadata"
+    --        (\result ->
+    --            case result of
+    --                Ok ( metadata, stars ) ->
+    --                    metadata.statusCode
+    --                        |> Expect.equal 200
+    --
+    --                _ ->
+    --                    Expect.fail ("Expected Ok, got: " ++ Debug.toString result)
+    --        )
     ]
         |> BackendTask.combine
         |> BackendTask.map (Test.describe "BackendTask tests")
