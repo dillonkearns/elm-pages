6539a2fda383b0f8c41dedbfe8c491dd270b468f
Dillon Kearns <dillon@incrementalelm.com>
2024-03-02 19:11:06 -0800

    Add some doc comments to an Effect.elm module.

--- examples/end-to-end/app/Effect.elm
+++ examples/end-to-end/app/Effect.elm
@@ -1,5 +1,11 @@
 module Effect exposing (Effect(..), batch, fromCmd, map, none, perform)
 
+{-|
+
+@docs Effect, batch, fromCmd, map, none, perform
+
+-}
+
 --import Form.FormData exposing (FormData)
 
 import Browser.Navigation
@@ -9,6 +15,7 @@ import Pages.Fetcher
 import Url exposing (Url)
 
 
+{-| -}
 type Effect msg
     = None
     | Cmd (Cmd msg)
@@ -26,27 +33,32 @@ type Effect msg
     | SubmitFetcher (Pages.Fetcher.Fetcher msg)
 
 
+{-| -}
 type alias RequestInfo =
     { contentType : String
     , body : String
     }
 
 
+{-| -}
 none : Effect msg
 none =
     None
 
 
+{-| -}
 batch : List (Effect msg) -> Effect msg
 batch =
     Batch
 
 
+{-| -}
 fromCmd : Cmd msg -> Effect msg
 fromCmd =
     Cmd
 
 
+{-| -}
 map : (a -> b) -> Effect a -> Effect b
 map fn effect =
     case effect of
@@ -82,6 +94,7 @@ map fn effect =
                 |> SubmitFetcher
 
 
+{-| -}
 perform :
     { fetchRouteData :
         { data : Maybe a
