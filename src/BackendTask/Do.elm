75ffcc67624459a1076f9a4110e44fd7a1256019
Dillon Kearns <dillon@incrementalelm.com>
2024-03-29 10:06:41 -0700

    Update docs.

--- src/BackendTask/Do.elm
+++ src/BackendTask/Do.elm
@@ -1,6 +1,9 @@
 module BackendTask.Do exposing
-    ( do, each, failIf, glob, log, noop, env
+    ( do
+    , noop
     , sh, exec
+    , glob, log, env
+    , each, failIf
     )
 
 {-|
@@ -11,9 +14,51 @@ module BackendTask.Do exposing
 Note that in order for this style to be usable, you'll need to use a special formatting script that allows you to use
 continuation style syntax without indenting each level in the continuation.
 
-@docs do, each, failIf, glob, log, noop, env
+
+## Custom Formatting Script
+
+It is a bit advanced and cumbersome, so beware before committing to this style. That said, here is a script you can use to
+apply continuation-style formatting to your Elm code:
+
+<https://gist.github.com/xarvh/1b65cb00e7240f1ccfa0bdbf30f97c62>
+
+You can see more discussion of continuation style in Elm in this Discourse post: <https://discourse.elm-lang.org/t/experimental-json-decoding-api/2121>.
+
+@docs do
+
+
+## Defining Your Own Continuation Utilities
+
+`do` is also helpful for building your own continuation-style utilities. For example, here is how [`glob`](#glob) is defined:
+
+    glob : String -> (List String -> BackendTask FatalError a) -> BackendTask FatalError a
+    glob pattern =
+        do <| Glob.fromString pattern
+
+To define helpers that have no resulting value, it is still useful to have an argument of `()` to allow the code formatter to
+recognize it as a continuation chain.
+
+    sh :
+        String
+        -> List String
+        -> (() -> BackendTask FatalError b)
+        -> BackendTask FatalError b
+    sh command args =
+        do <| Shell.sh command args
+
+@docs noop
+
+
+## Shell Commands
 
 @docs sh, exec
+
+
+## Common Utilities
+
+@docs glob, log, env
+
+@docs each, failIf
 
 -}
 
@@ -31,7 +76,8 @@ log string then_ =
     do (Script.log string) then_
 
 
-{-| -}
+{-| Use any `BackendTask` into a continuation-style task.
+-}
 do : BackendTask error a -> (a -> BackendTask error b) -> BackendTask error b
 do fn requestInfo =
     BackendTask.andThen requestInfo fn
@@ -43,13 +89,41 @@ noop =
     BackendTask.succeed ()
 
 
-{-| -}
+{-| A continuation-style helper for [`Glob.fromString`](BackendTask-Glob#fromString).
+
+In a shell script, you can think of this as a stand-in for globbing files directly within a command. All commands in
+you run with [`BackendTask.Shell`](BackendTask-Shell) (including the [`sh`](#sh) and [`exec`](#exec) helpers)
+sanitizes and escapes all arguments passed, and does not do glob expansion, so this is helpful for translating
+a shell script to Elm.
+
+This example passes a list of matching file paths along to an `rm -f` command.
+
+    example : BackendTask FatalError ()
+    example =
+        glob "src/**/*.elm" <| \elmFiles ->
+        log ("You have " ++ String.fromInt (List.length elmFiles) ++ " Elm files") <| \() ->
+        noop
+
+-}
 glob : String -> (List String -> BackendTask FatalError a) -> BackendTask FatalError a
-glob pattern then_ =
-    do (Glob.fromString pattern) then_
+glob pattern =
+    do <| Glob.fromString pattern
 
 
-{-| -}
+{-|
+
+    checkCompilationInDir : String -> BackendTask FatalError ()
+    checkCompilationInDir dir =
+        glob (dir ++ "/**/*.elm") <| \elmFiles ->
+        each elmFiles
+            (\elmFile ->
+                Shell.sh "elm" [ "make", elmFile, "--output", "/dev/null" ]
+                    |> BackendTask.quiet
+            )
+        <| \_ ->
+        noop
+
+-}
 each : List a -> (a -> BackendTask error b) -> (List b -> BackendTask error c) -> BackendTask error c
 each list fn then_ =
     do
