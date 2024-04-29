86b67d9749ab0c9f9423cff61d049e123e44d0a9
Dillon Kearns <dillon@incrementalelm.com>
2024-04-29 11:56:43 -0700

    Try npm ci in test script.

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@ set -ex;
 yes | lamdera reset || true
 npm run build:generator
 npx elm-test-rs --compiler lamdera
-(cd examples/routing && yes | lamdera reset || true && npm i && npm run build && npx elm-test-rs --compiler lamdera)
+(cd examples/routing && yes | lamdera reset || true && npm ci && npm run build && npx elm-test-rs --compiler lamdera)
 (cd generator/dead-code-review && npx elm-test-rs --compiler lamdera)
 (cd generator/review && npx elm-test-rs --compiler lamdera)
 npm run test:snapshot
