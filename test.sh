73e4030add2d426e75e36d711a6f2a2261baca56
Dillon Kearns <dillon@incrementalelm.com>
2024-04-29 10:29:25 -0700

    Remove elmi-to-json call in test script.

--- test.sh
+++ test.sh
@@ -6,6 +6,5 @@ npx elm-test-rs --compiler lamdera
 (cd generator/dead-code-review && npx elm-test-rs --compiler lamdera)
 (cd generator/review && npx elm-test-rs --compiler lamdera)
 npm run test:snapshot
-npx elmi-to-json --version
 elm-verify-examples --run-tests --elm-test-args '--compiler=lamdera'
 (cd generator && vitest run)
