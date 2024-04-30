2fb2d49fa490415378cb27cdff69f0850d02c6ec
Dillon Kearns <dillon@incrementalelm.com>
2024-04-29 22:08:15 -0700

    Add rollup linux at top-level in script.

--- test.sh
+++ test.sh
@@ -1,6 +1,7 @@
 set -ex;
 yes | lamdera reset || true
 npm run build:generator
+npm install --save-dev @rollup/rollup-linux-x64-gnu
 npx elm-test-rs --compiler lamdera
 (cd examples/routing && yes | lamdera reset || true && npm install --save-dev @rollup/rollup-linux-x64-gnu && npm install && npm run build && npx elm-test-rs --compiler lamdera)
 (cd generator/dead-code-review && npx elm-test-rs --compiler lamdera)
