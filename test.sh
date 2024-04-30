ac5f01f2fd0857fb7fcdb7d0961306610a5c44c6
Dillon Kearns <dillon@incrementalelm.com>
2024-04-29 20:54:42 -0700

    Update package-lock for wasm.

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@ set -ex;
 yes | lamdera reset || true
 npm run build:generator
 npx elm-test-rs --compiler lamdera
-(cd examples/routing && yes | lamdera reset || true && rm -rf node_modules package-lock.json && npm install && npm run build && npx elm-test-rs --compiler lamdera)
+(cd examples/routing && yes | lamdera reset || true && npm ci && npm run build && npx elm-test-rs --compiler lamdera)
 (cd generator/dead-code-review && npx elm-test-rs --compiler lamdera)
 (cd generator/review && npx elm-test-rs --compiler lamdera)
 npm run test:snapshot
