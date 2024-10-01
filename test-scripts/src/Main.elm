448b0fe657f334a243a46f4387a54877ebda5067
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2024-10-01 14:05:59 +0200

    Cleanup

--- test-scripts/src/Main.elm
+++ test-scripts/src/Main.elm
@@ -1,8 +1,7 @@
 module Main exposing (run)
 
-import BackendTask exposing (BackendTask)
+import BackendTask
 import BackendTask.Custom
-import FatalError exposing (FatalError)
 import Json.Decode
 import Json.Encode
 import Pages.Script as Script exposing (Script)
