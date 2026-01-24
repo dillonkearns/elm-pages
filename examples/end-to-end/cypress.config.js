a24bfe2d8005e5927e31dba6e22e5c431b66fd55
Dillon Kearns <dillon@incrementalelm.com>
2026-01-23 21:04:40 -0800

    Remove transpiled cypress config file.

--- examples/end-to-end/cypress.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "cypress";
-export default defineConfig({
-    defaultCommandTimeout: 20000,
-    e2e: {
-        setupNodeEvents(on, config) { },
-        baseUrl: "http://localhost:1234",
-    },
-});
