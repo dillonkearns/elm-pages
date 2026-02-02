564d239cebe1620685b164cb9d753f272d878db6
Dillon Kearns <dillon@incrementalelm.com>
2026-02-02 05:33:18 -0800

    Use netlify adapter for docs site to test serverless function.

--- examples/docs/elm-pages.config.mjs
+++ examples/docs/elm-pages.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import { ViteWebfontDownload } from "vite-plugin-webfont-dl";
+import adapter from "../../adapter/netlify.js";
 
 export default {
   vite: defineConfig({
@@ -11,6 +12,7 @@ export default {
       ]),
     ],
   }),
+  adapter,
   headTagsTemplate(context) {
     return `
 <link rel="stylesheet" href="/style.css" />
