b13de95225597d07b374f6397ca9dffb98208046
Dillon Kearns <dillon@incrementalelm.com>
2026-02-02 22:38:23 -0800

    Don't run StaticRegionScope rule in dev server, we are now de-optimizing View.freeze code that is tainted with model (may publish elm-review rule for users to opt in to).

--- generator/review/src/ReviewConfig.elm
+++ generator/review/src/ReviewConfig.elm
@@ -1,13 +1,11 @@
 module ReviewConfig exposing (config)
 
 import Pages.Review.NoContractViolations
-import Pages.Review.StaticRegionScope
 import Review.Rule as Rule exposing (Rule)
 
 
 config : List Rule
 config =
     [ Pages.Review.NoContractViolations.rule
-    , Pages.Review.StaticRegionScope.rule
     ]
 
