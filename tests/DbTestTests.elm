940fc3bd528db992a8f76de11f5cbd28ad55c5b7
Dillon Kearns <dillon@incrementalelm.com>
2026-03-09 20:57:27 -0700

    Use default seed from Db in Test.BackendTask, with a helper to set an initial db state instead.

--- tests/DbTestTests.elm
+++ tests/DbTestTests.elm
@@ -14,7 +14,7 @@ import Test.BackendTask as BackendTaskTest
 all : Test
 all =
     describe "DB virtual layer"
-        [ describe "fromBackendTaskWithDb"
+        [ describe "withDb"
             [ test "script that reads DB gets the seeded value" <|
                 \() ->
                     FakeDb.get
@@ -22,15 +22,19 @@ all =
                             (\db ->
                                 Script.log ("counter=" ++ String.fromInt db.counter)
                             )
-                        |> BackendTaskTest.fromBackendTaskWithDb FakeDb.testConfig
-                            { counter = 42, name = "test" }
+                        |> BackendTaskTest.fromBackendTaskWith
+                            (BackendTaskTest.init
+                                |> BackendTaskTest.withDbSetTo { counter = 42, name = "test" } FakeDb.testConfig
+                            )
                         |> BackendTaskTest.ensureStdout [ "counter=42" ]
                         |> BackendTaskTest.expectSuccess
             , test "script that updates DB persists the change" <|
                 \() ->
                     FakeDb.update (\db -> { db | counter = db.counter + 1 })
-                        |> BackendTaskTest.fromBackendTaskWithDb FakeDb.testConfig
-                            { counter = 10, name = "test" }
+                        |> BackendTaskTest.fromBackendTaskWith
+                            (BackendTaskTest.init
+                                |> BackendTaskTest.withDbSetTo { counter = 10, name = "test" } FakeDb.testConfig
+                            )
                         |> BackendTaskTest.expectDb FakeDb.testConfig
                             (\db -> Expect.equal 11 db.counter)
             , test "two consecutive updates accumulate" <|
@@ -40,23 +44,29 @@ all =
                             (\() ->
                                 FakeDb.update (\db -> { db | counter = db.counter * 10 })
                             )
-                        |> BackendTaskTest.fromBackendTaskWithDb FakeDb.testConfig
-                            { counter = 5, name = "" }
+                        |> BackendTaskTest.fromBackendTaskWith
+                            (BackendTaskTest.init
+                                |> BackendTaskTest.withDbSetTo { counter = 5, name = "" } FakeDb.testConfig
+                            )
                         |> BackendTaskTest.expectDb FakeDb.testConfig
                             (\db -> Expect.equal 60 db.counter)
-            , test "expectDb with default seed" <|
+            , test "withDb uses seed from testConfig" <|
                 \() ->
                     FakeDb.update (\db -> { db | counter = db.counter + 1 })
-                        |> BackendTaskTest.fromBackendTaskWithDb FakeDb.testConfig
-                            FakeDb.testConfig.seed
+                        |> BackendTaskTest.fromBackendTaskWith
+                            (BackendTaskTest.init
+                                |> BackendTaskTest.withDb FakeDb.testConfig
+                            )
                         |> BackendTaskTest.expectDb FakeDb.testConfig
                             (\db -> Expect.equal 1 db.counter)
             , test "returned values can be asserted after DB setup" <|
                 \() ->
                     FakeDb.update (\db -> { db | counter = db.counter + 1 })
                         |> BackendTask.andThen (\() -> FakeDb.get)
-                        |> BackendTaskTest.fromBackendTaskWithDb FakeDb.testConfig
-                            { counter = 10, name = "test" }
+                        |> BackendTaskTest.fromBackendTaskWith
+                            (BackendTaskTest.init
+                                |> BackendTaskTest.withDbSetTo { counter = 10, name = "test" } FakeDb.testConfig
+                            )
                         |> BackendTaskTest.expectSuccessWith
                             (\db -> Expect.equal 11 db.counter)
             , test "DB + HTTP simulation combined" <|
@@ -69,8 +79,10 @@ all =
                             (\count ->
                                 FakeDb.update (\db -> { db | counter = db.counter + count })
                             )
-                        |> BackendTaskTest.fromBackendTaskWithDb FakeDb.testConfig
-                            { counter = 0, name = "" }
+                        |> BackendTaskTest.fromBackendTaskWith
+                            (BackendTaskTest.init
+                                |> BackendTaskTest.withDbSetTo { counter = 0, name = "" } FakeDb.testConfig
+                            )
                         |> BackendTaskTest.simulateHttpGet
                             "https://api.example.com/count"
                             (Json.Encode.object [ ( "value", Json.Encode.int 7 ) ])
@@ -87,15 +99,19 @@ all =
                                     }
                                     |> BackendTask.allowFatal
                             )
-                        |> BackendTaskTest.fromBackendTaskWithDb FakeDb.testConfig
-                            { counter = 99, name = "" }
+                        |> BackendTaskTest.fromBackendTaskWith
+                            (BackendTaskTest.init
+                                |> BackendTaskTest.withDbSetTo { counter = 99, name = "" } FakeDb.testConfig
+                            )
                         |> BackendTaskTest.ensureFile "output.txt" "counter=99"
                         |> BackendTaskTest.expectSuccess
             , test "string fields round-trip through DB" <|
                 \() ->
                     FakeDb.update (\db -> { db | name = "hello world" })
-                        |> BackendTaskTest.fromBackendTaskWithDb FakeDb.testConfig
-                            { counter = 0, name = "" }
+                        |> BackendTaskTest.fromBackendTaskWith
+                            (BackendTaskTest.init
+                                |> BackendTaskTest.withDbSetTo { counter = 0, name = "" } FakeDb.testConfig
+                            )
                         |> BackendTaskTest.expectDb FakeDb.testConfig
                             (\db -> Expect.equal "hello world" db.name)
             ]
@@ -109,7 +125,7 @@ all =
                             (\msg ->
                                 Expect.all
                                     [ \m -> Expect.equal True (String.contains "Pages.Db" m)
-                                    , \m -> Expect.equal True (String.contains "fromBackendTaskWithDb" m)
+                                    , \m -> Expect.equal True (String.contains "withDb" m)
                                     ]
                                     msg
                             )
